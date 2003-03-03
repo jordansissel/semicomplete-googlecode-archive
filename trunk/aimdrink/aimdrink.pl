11b60e0d43a115acd643069490a628495f811450
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2003-03-03 07:25:13 +0000

    Added cowsay support Added greeting support
    git-svn-id: http://semicomplete.googlecode.com/svn@442 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/aimdrink/aimdrink.pl
+++ trunk/aimdrink/aimdrink.pl
@@ -10,10 +10,12 @@ use IPC::Open2;
 
 require "userdata.pl";
 
+my $DEBUG = 3;
+
 my $DRINKHOST = "drink.csh.rit.edu";
 my $DRINKPORT = 4242;
 
-my ($userdata, $clientinfo);
+my ($userdata, $clientinfo, $infotime);
 my %session;
 my ($aim,$conn);
 my $s = IO::Select->new();
@@ -22,8 +24,8 @@ $userdata = Drink::read_users();
 
 $aim = new Net::AIM;
 $aim->debug(0);
-$aim->newconn(Screenname => 'CSH Drink', Password => 'cshdrink123');
-#$aim->newconn(Screenname => 'CSH Drink2', Password => 'cshdrink123');
+#$aim->newconn(Screenname => 'CSH Drink', Password => 'cshdrink123');
+$aim->newconn(Screenname => 'CSH Drink2', Password => 'cshdrink123');
 #$aim->newconn(Screenname => 'mobilesoutherner', Password => 'carleneway');
 
 #Handlers
@@ -74,7 +76,7 @@ sub read_from_sessions {
       }
 
       print "Drink> $data\n";
-      sendim("$key","Drink> $data");
+      #sendim("$key","Drink> $data");
    }
 
 }
@@ -88,20 +90,23 @@ sub sendim {
 
 sub translate {
    my ($from,$msg) = @_;
-   my $language = $session{$from}{'data'}->{'language'};
+   my ($language,$parm) = split(m/\s+/,$session{$from}{'data'}->{'language'},2);
    print "Language: " . $language . "\n";
 
-   my @list = ('b1ff','brooklyn','chef','cockney','drawl','fudd','funetak','jethro','jive','kraut','pansy','postmodern','redneck','valspeak','warez');
+   my @list = ('b1ff','brooklyn','chef','cockney','drawl','fudd','funetak','jethro','jive','kraut','pansy','postmodern','redneck','valspeak','warez','cowsay','cowthink');
 
    my ($lang) = grep(m/^$language$/i,@list);
    if ($lang) {
-      my ($r,$w);
-      my ($pid) = open2($r,$w,"/usr/local/bin/$language") or die "Unable to open filter, $language";
+      my ($r,$w,$opts);
+      $opts = "-n" if ($language =~ m/cow(say|think)/i);
+      $opts = " -f $parm" if ( ($language =~ m/cow(say|think)/i) && (defined($parm)) );
+      my ($pid) = open2($r,$w,"/usr/local/bin/$language $opts") or die "Unable to open filter, $language";
       print $w $msg;
       close($w);
       $msg = "";
       while ($_ = readline($r)) { $msg .= $_ };
       #$msg = readline($r);
+      $msg = "\n<pre>$msg</pre>" if ($language =~ m/cow(say|think)/i);
    }
 
    return $msg;
@@ -122,8 +127,8 @@ sub on_error {
    my ($hash,$event,$from,$to) = @_;
    print STDERR "$from sent error code #".$event->{'args'}->[0] . "\n";
    print STDERR $event->trans($event->{'args'}->[0]) . "\n";
-   print STDERR "Exiting...\n";
-   exit 1;
+   #print STDERR "Exiting...\n";
+   #exit 1;
 }
 
 sub on_warn {
@@ -139,6 +144,32 @@ sub handle_message {
    $msg =~ s/\<.*?\>//g;
 
    print "$from: $msg\n";
+   unless (defined($session{$from})) {
+      print "-> Creating new session for $from\n";
+      #New session
+      my ($sock, $dconn);
+
+      $sock = Symbol::gensym();
+
+      socket($sock,PF_INET,SOCK_STREAM,getprotobyname('tcp')) 
+        or handle_error("Unable to create new socket: $!",1,$from);
+      $dconn = connect($sock,sockaddr_in($DRINKPORT, inet_aton($DRINKHOST)));
+
+      $session{$from} = { socket => $sock, conn => $dconn };
+      $session{$from}{'data'} = Drink::find_user($userdata,$from);
+
+      print "Session ($from): " . $session{$from}{'data'} ."\n";
+
+      #Selection adding
+      $s->add($sock);
+
+      if (defined($session{$from}{'data'}->{'greet'})) {
+	 my $greet = $session{$from}{'data'}->{'greet'};
+	 sendim($from,substr(`$greet`,0,-1));
+      } else {
+	 sendim($from,"\nHello, $from!\nType 'help' for help.");
+      }
+   }
    if (defined($session{$from})) {
       if ($msg =~ /^h(e(l(p)?)?)?$/) {
 	 sendim($from, "This is the AIM Drink Client! The following commands are available: \n     Status\n     Drop\nAlso feel free to type 'help <command>'");
@@ -152,14 +183,19 @@ sub handle_message {
 	 if ($3 =~ m/^l(a(n(g(u(a(g(e)?)?)?)?)?)?)? (.*)$/i) { #set language
 	    print "New language for $from: $8\n";
 	    $session{$from}{'data'}->{'language'} = $8;
+	 } elsif ($3 =~ m/^g(r(e(e(t)?)?)?)? (.*)$/i) {
+	    print "New greeting for $from: $5\n";
+	    $session{$from}{'data'}->{'greet'} = $5;
 	 }
       } elsif ($msg =~ /^s(t(a(t(u(s)?)?)?)?)?$/i) { #status
-	 get_drink_stats($session{$from}{'socket'});
+	 get_drink_stats($session{$from}{'socket'}) 
+	    if ($infotime + 600 < time());
 	 print "Status: $clientinfo\n";
 	 sendim($from,$clientinfo);
       } elsif ($msg =~ /^d(r(o(p)?)?)?$/i) { #drop
 	 sendim($from,"Not implemented yet.");
       } elsif ($msg =~ /^s(a(v(e)?)?)?$/i) { #save
+	 print "Data: $userdata\n";
 	 Drink::write_users($userdata); 
       } elsif ($msg =~ /^i(n(f(o)?)?)?$/i) { #info
 	 my $say;
@@ -169,26 +205,6 @@ sub handle_message {
       }
       
       #send($session{$from}{'socket'},$msg . "\n",0);
-   }
-   else {
-      print "-> Creating new session for $from\n";
-      #New session
-      my ($sock, $dconn);
-
-      $sock = Symbol::gensym();
-
-      socket($sock,PF_INET,SOCK_STREAM,getprotobyname('tcp')) 
-        or handle_error("Unable to create new socket: $!",1,$from);
-      $dconn = connect($sock,sockaddr_in($DRINKPORT, inet_aton($DRINKHOST)));
-
-      $session{$from} = { socket => $sock, conn => $dconn };
-      $session{$from}{'data'} = $userdata->{$from};
-
-      print "Session ($from): " . $session{$from}{'data'} ."\n";
-
-      #Selection adding
-      $s->add($sock);
-      sendim($from,"\nHello, $from!\nType 'help' for help.");
    }
 
 }
@@ -230,7 +246,8 @@ sub get_drink_stats {
    while (1) {
       ($ready) = $sel->can_read(1);
       last unless (defined($ready));
-      $brec = recv($ready,$line,8192,0);
+      #$brec = recv($ready,$line,8192,0);
+      $line = readline($ready);
       next if ($line =~ m/^OK/);
       last if (length($line) == 0);
       $data = $frag . $line;
@@ -247,15 +264,13 @@ sub get_drink_stats {
    $info = '';
    foreach (@slots) {
      my @word = split(" ",$_);
-     $info .= '<b>'.$word[1].'</b>: $0.'.$word[2].' - '.$word[3].' left.<br>';
+     $info .= '<b>'.$word[1].'</b>: $0.'.$word[2].' - '.$word[3]." left.\n";
    }
 
    $clientinfo = $info;
+   $infotime = time();
    $aim->set_info($info);
 }
-
-
-###
 
 sub include {
    my ($file) = @_;
