7200bac97ab4ed25e41755d2e9b041490a46cf32
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2003-12-29 03:19:38 +0000

    - Instead of hashes, we use constants now.
    git-svn-id: http://semicomplete.googlecode.com/svn@470 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/tic/Tic/Constants.pm
+++ trunk/tic/Tic/Constants.pm
@@ -1,14 +1,21 @@
 package Tic::Constants;
 
 use Exporter;
+use constant KEY_CONSTANTS => {
+		"\e[A"    =>     "UP",
+		"\e[B"    =>     "DOWN",
+		"\e[C"    =>     "RIGHT",
+		"\e[D"    =>     "LEFT",
+	};
+
+
 @ISA = qw(Exporter);
+@EXPORT_OK = qw(KEY_CONSTANTS);
 
-@EXPORT qw(%KEY_CONSTANTS)
 
-my %KEY_CONSTANTS {
-	"\e[A"    =>     "UP",
-	"\e[D"    =>     "LEFT"
-	"\e[C"    =>     "RIGHT",
-	"\e[D"    =>     "DOWN",
+sub import {
+	Tic::Constants->export_to_level(1,@_);
 }
 
+# true dat...
+1;
