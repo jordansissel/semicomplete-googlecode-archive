b9d9fc50154838575985511122cfac79424e79d1
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-10 22:39:24 +0000

    initial
    git-svn-id: http://semicomplete.googlecode.com/svn@501 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/datacomm/src/BattleshipPlayer.java
@@ -0,0 +1,34 @@
+/*
+ * BattleshipPlayer.java
+ * 
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/10 22:39:24  tristan
+ *   initial
+ *
+ */
+
+/**
+ * Starts a battleship player client.
+ * 
+ * @author tristan
+ */
+public class BattleshipPlayer {
+    /**
+     * Creates a new BattleshipPlayer
+     */
+    public BattleshipPlayer() {
+
+    }
+
+    /**
+     * Starts up the program.
+     * @param args The command line arguments.
+     */
+    public static void main( String[] args ) {
+        
+    }
+}   // BattleshipPlayer
---
+++ trunk/datacomm/src/BattleshipServer.java
@@ -0,0 +1,34 @@
+/*
+ * BattleshipServer.java
+ * 
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/10 22:38:21  tristan
+ *   initial
+ *
+ */
+
+/**
+ * The main method that starts the battle ship server.
+ * 
+ * @author tristan
+ */
+public class BattleshipServer {
+    /**
+     * Creates a new BattleshipServer
+     */
+    public BattleshipServer() {
+
+    }
+
+    /**
+     * Starts up the program.
+     * @param args The command line arguments.
+     */
+    public static void main( String[] args ) {
+
+    }
+}   // BattleshipServer
