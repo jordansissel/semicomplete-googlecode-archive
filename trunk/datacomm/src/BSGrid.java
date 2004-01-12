26d6c662f8aeb410ca283ab4982756063dd790bf
njohnson <njohnson@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-12 21:49:15 +0000

    added a toString method, so we can print the grids and translate them easily to pass over the network.
    git-svn-id: http://semicomplete.googlecode.com/svn@509 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/BSGrid.java
+++ trunk/datacomm/src/BSGrid.java
@@ -6,14 +6,19 @@
  *
  * Revisions:
  * 	$Log$
+ * 	Revision 1.4  2004/01/12 21:49:15  njohnson
+ * 	added a toString method, so we can print the grids and translate them
+ * 	easily to pass over the network.
+ *
  * 	Revision 1.3  2004/01/12 04:51:41  njohnson
  * 	I made the code compilation error free. Still mostly stubs.
- *
+ * 	
  * 	Revision 1.1  2004/01/12 02:29:50  njohnson
  * 	initial commit
  * 	
  */
 
+import java.lang.Math;
 
 /**
  * The grid objects to represent oceans for each player in BattleShip
@@ -21,19 +26,35 @@
  * @author Nicholas R. Johnson - nrj7604 
  */
 public class BSGrid {
-
-	BSShip[] fleet = new BSShip[ 5 ]; 
+	private BSShip[] fleet; 
+	private byte[][] grid; 
+	private String name;
 
 	public BSGrid( String id ) {
-
-
+		name = id;
+		generateGrid();
 	} // constructor
 
-	public byte[] generateGrid() {
-		byte[] newGrid = new byte[10];
+	public void generateGrid() {
+		byte[][] newGrid;
+		newGrid = new byte[10][10];
+		int valueX;
+		int valueY;
+
+		valueX = (int)Math.ceil( Math.random()*10 );	
+
+		
 
 		return newGrid;
 	} //generateGrid()
+	
+	public void generateGrid( byte[][] newGrid ) {
+		grid = newGrid;
+	} //generateGrid()
+
+	public String toString() {
+
+	} //toString()
 
 } // BSGrid
 
