06607d1d9b93745937a31c681c0f4b93c3507afd
njohnson <njohnson@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-12 02:29:50 +0000

    initial commit
    git-svn-id: http://semicomplete.googlecode.com/svn@504 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/datacomm/src/BSGrid.java
@@ -0,0 +1,33 @@
+/**
+ * BSGrid.java
+ *
+ * Version:
+ * 	$Id$
+ *
+ * Revisions:
+ * 	$Log$
+ * 	Revision 1.1  2004/01/12 02:29:50  njohnson
+ * 	initial commit
+ *
+ */
+
+
+/**
+ * The grid objects to represent oceans for each player in BattleShip
+ *
+ * @author Nicholas R. Johnson - nrj7604 
+ */
+public class BSGrid {
+
+	BSShip[] fleet = new BSShip[ 5 ]; 
+
+	public BSGrid( String id ) {
+
+
+	} // constructor
+
+	public int[] 
+
+} // BSGrid
+
+
