604184d5926af035aa32bf9f29e9e628a4ea9c2a
njohnson <njohnson@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-12 23:21:38 +0000

    completed the toString() method so that Grids can easily be printed to the terminal
    git-svn-id: http://semicomplete.googlecode.com/svn@511 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/BSGrid.java
+++ trunk/datacomm/src/BSGrid.java
@@ -6,10 +6,14 @@
  *
  * Revisions:
  * 	$Log$
+ * 	Revision 1.5  2004/01/12 23:21:38  njohnson
+ * 	completed the toString() method so that Grids can easily be
+ * 	printed to the terminal
+ *
  * 	Revision 1.4  2004/01/12 21:49:15  njohnson
  * 	added a toString method, so we can print the grids and translate them
  * 	easily to pass over the network.
- *
+ * 	
  * 	Revision 1.3  2004/01/12 04:51:41  njohnson
  * 	I made the code compilation error free. Still mostly stubs.
  * 	
@@ -44,16 +48,47 @@ public class BSGrid {
 		valueX = (int)Math.ceil( Math.random()*10 );	
 
 		
-
-		return newGrid;
 	} //generateGrid()
 	
 	public void generateGrid( byte[][] newGrid ) {
 		grid = newGrid;
 	} //generateGrid()
 
+	/**
+	 * Creates a string that is the BattleShip grid in 
+	 * human readable form.
+	 *
+	 * @return - readable grid
+	 */
 	public String toString() {
+		String gridString = new String();
+		String[] letters = { "A", "B", "C", "D", "E", 
+		"F", "G", "H", "I", "J" };
+		int j = 0;
 
+		//add top area
+		gridString = gridString.concat(
+		" 0123456789\n" );
+
+		for( int i = 0; i < 10; i++ ) {
+			gridString = 
+			gridString.concat( letters[ i ] );	
+			for( j = 0; j < 10; j++ ) {
+				if( grid[ i ][ j ] == 0 ) { //empty
+					gridString = gridString.concat( "." );
+				} else if( grid[ i ][ j ] == 1 ) { //ship
+					gridString = gridString.concat( "0" );
+				} else if( grid[ i ][ j ] == 2 ) { //miss
+					gridString = gridString.concat( "*" );
+				} else { //hit
+					gridString = gridString.concat( "X" );
+				}
+
+			}
+			gridString = gridString.concat( "\n" );
+		}
+
+		return gridString;
 	} //toString()
 
 } // BSGrid
