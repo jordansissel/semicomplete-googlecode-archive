54095e1fd18afacdd27a1200cbfcce78602cac27
njohnson <njohnson@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-12 03:29:02 +0000

    initial commit
    git-svn-id: http://semicomplete.googlecode.com/svn@506 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/datacomm/src/BSShip.java
@@ -0,0 +1,45 @@
+/**
+ * BSShip.java
+ *
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/12 03:29:02  njohnson
+ *   initial commit
+ *
+ *
+ */
+
+/**
+ * A class that contains the endpoints of a ship in 
+ * a BattleShip fleet
+ *
+ * @author Nicholas R. Johnson
+ */
+public class BSShip {
+	private byte[] = new byte[4];	
+
+	public BSShip( byte startPointX, 
+			byte startPointY,		
+			byte endPointX,
+			byte endPointY ) {
+
+
+
+
+	} // constructor
+
+	public byte[] getStart() {
+
+
+	}
+
+	public byte[] getEnd() {
+
+	}
+
+} //BSShip
+
+
