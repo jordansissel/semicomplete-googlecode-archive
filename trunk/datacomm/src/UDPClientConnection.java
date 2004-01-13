488d9bea70b7fde28c049e5737a424c5dbb9bd3e
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-13 18:03:12 +0000

    added packet generation to connect method. untested
    git-svn-id: http://semicomplete.googlecode.com/svn@525 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/UDPClientConnection.java
+++ trunk/datacomm/src/UDPClientConnection.java
@@ -6,6 +6,9 @@
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.5  2004/01/13 18:03:12  tristan
+ *   added packet generation to connect method. untested
+ *
  *   Revision 1.4  2004/01/13 14:55:00  tristan
  *   started connect method.
  *
@@ -23,6 +26,10 @@
 import java.net.InetAddress;
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.ByteArrayInputStream;
 
 /**
  * Manages a UDP based connection with the server
@@ -33,11 +40,14 @@ public class UDPClientConnection extends ClientConnection {
     // constants
     private static final long RETRY_DELAY = 5 * 1000;
     private static final int RETRY_COUNT = 3;
-    private static final int BUFFER_SIZE = 1024;
     private static final int TIMEOUT = 30 * 1000;
+    private static final int BUFFER_SIZE = 1024;
 
     // member variables
     private DatagramSocket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private ByteArrayOutputStream buffer;
 
     /**
      * Creates a new UDPClientConnection
@@ -51,6 +61,12 @@ public class UDPClientConnection extends ClientConnection {
             System.err.println( e.getMessage() );
             e.printStackTrace();
         }
+
+        // initialize byte streams
+        buffer = new ByteArrayOutputStream( BUFFER_SIZE );
+        out = new DataOutputStream( buffer );
+        in = new DataInputStream( new ByteArrayInputStream(
+                                  buffer.toByteArray() ) );
     }
 
     /**
@@ -85,17 +101,29 @@ public class UDPClientConnection extends ClientConnection {
 
         // attempt to send a hello to the server
         socket.connect( getServerHost(), getServerPort() );
-        byte[] buffer = new byte[ BUFFER_SIZE ];
-        DatagramPacket packet = new DatagramPacket( buffer, buffer.length );
-        try {
-            // TODO create special hello packet
-            socket.send( packet );
-            socket.setSoTimeout( TIMEOUT );
-            socket.receive( packet );
-            retVal = true;
-        } catch ( Exception e ) {
-            System.err.println( e.getMessage() );
-            e.printStackTrace();
+        DatagramPacket packet = new DatagramPacket( buffer.toByteArray(),
+                                                    buffer.size() );
+
+        for ( int i = 0; i < RETRY_COUNT && !retVal; i++ ) {
+            try {
+                // set msg
+                buffer.reset();
+                out.writeInt( HELLO_MSG );
+
+                // send package
+                socket.send( packet );
+                socket.setSoTimeout( TIMEOUT );
+
+                // test response
+                socket.receive( packet );
+                int response = in.readInt();
+                if ( response == HELLO_RESPONSE ) {
+                    retVal = true;
+                }
+            } catch ( Exception e ) {
+                System.err.println( e.getMessage() );
+                e.printStackTrace();
+            }
         }
 
         // update status
