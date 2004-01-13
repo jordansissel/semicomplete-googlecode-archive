74bf60251c451b59202cafee46b067cf95964f71
njohnson <njohnson@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-13 22:56:23 +0000

    cleaned up the generateGrid code by putting the overlap finding algorithm in another method. Will clean up tabs for Tristan soon with a script.
    git-svn-id: http://semicomplete.googlecode.com/svn@528 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/BSGrid.java
+++ trunk/datacomm/src/BSGrid.java
@@ -6,9 +6,14 @@
  *
  * Revisions:
  * 	$Log$
+ * 	Revision 1.9  2004/01/13 22:56:23  njohnson
+ * 	cleaned up the generateGrid code by putting the overlap finding
+ * 	algorithm in another method. Will clean up tabs for Tristan soon
+ * 	with a script.
+ *
  * 	Revision 1.8  2004/01/13 03:46:49  tristan
  * 	removed useless import
- *
+ * 	
  * 	Revision 1.7  2004/01/13 02:34:33  tristan
  * 	fixed up to string with string buffer.
  * 	
@@ -40,62 +45,62 @@
  * @author Nicholas R. Johnson - nrj7604
  */
 public class BSGrid {
-	private BSShip[] fleet;
-	private byte[][] grid;
-	private String name;
+    private BSShip[] fleet;
+    private byte[][] grid;
+    private String name;
 
     /**
      *
      * @param id
      */
-	public BSGrid( String id ) {
-		name = id;
-		fleet = new BSShip[ 4 ];
-		generateGrid();
-	} // constructor
+    public BSGrid( String id ) {
+        name = id;
+        fleet = new BSShip[ 4 ];
+        generateGrid();
+    } // constructor
 
     /**
      *
      */
-	public void generateGrid() {
-		byte[][] newGrid;
-		newGrid = new byte[10][10];
-		int startRowValue = 0;
-		int startColValue = 0;
-		int endRowValue = 0;
-		int endColValue = 0;
-		boolean overlap = false; //true if there is a ship overlap
-		int p = 0;
-		int shipL = 0; // the ship length;
-		int dir = 0; // direction from start to end of ship
-		            //
-			    //		1
-			    //	2	+	4
-			    //		3
-			    //
+    public void generateGrid() {
+        byte[][] newGrid;
+        newGrid = new byte[10][10];
+        int startRowValue = 0;
+        int startColValue = 0;
+        int endRowValue = 0;
+        int endColValue = 0;
+        boolean overlap = false; //true if there is a ship overlap
+        int p = 0;
+        int shipL = 0; // the ship length;
+        int dir = 0; // direction from start to end of ship
+                     //
+                     //		1
+                     //	2	+	4
+                     //		3
+                     //
 
-		for( int i = 0; i < 5; i++ ) {
+        for( int i = 0; i < 5; i++ ) {
 
-			//generate the random values for the ship's
-			//start position
-			startRowValue = (int)
-			( Math.ceil( Math.random()*10 ) - 1.0 );
-			startColValue = (int)
-			( Math.ceil( Math.random()*10 ) - 1.0 );
+        //generate the random values for the ship's
+        //start position
+        startRowValue = (int)
+        ( Math.ceil( Math.random()*10 ) - 1.0 );
+        startColValue = (int)
+        ( Math.ceil( Math.random()*10 ) - 1.0 );
 
-			//gotta set the ship's length
-			switch( i ) {
-				case 0:
-					shipL = 2;
-					break;
-				case 1:
-					shipL = 3;
-					break;
-				case 2:
-					shipL = 3;
-					break;
-				case 3:
-					shipL = 4;
+        //gotta set the ship's length
+        switch( i ) {
+            case 0:
+                shipL = 2;
+                break;
+            case 1:
+                shipL = 3;
+                break;
+            case 2:
+                shipL = 3;
+                break;
+            case 3:
+                shipL = 4;
 					break;
 				case 4:
 					shipL = 5;
@@ -117,46 +122,10 @@ public class BSGrid {
 			}
 
 			//initially check for ship overlap
-			switch( dir ) {
-				case 1:
-				for( p = 0; p < shipL; p++ ) {
-					if(grid[startRowValue+p][startColValue] != 0) {
-						overlap = true;
-					}
-				}
-				endRowValue=startRowValue+shipL;
-				endColValue = startColValue;
-				break;
-				case 2:
-				for( p = 0; p < shipL; p++ ) {
-					if(grid[startRowValue][startColValue-p] != 0 ) {
-						overlap = true;
-					}
-				}
-				endRowValue = startRowValue;
-				endColValue=startColValue-shipL;
-				break;
-				case 3:
-				for( p = 0; p < shipL; p++ ) {
-					if(grid[startRowValue-p][startColValue] != 0 ) {
-						overlap = true;
-					}
-				}
-				endRowValue=startRowValue-shipL;
-				endColValue = startColValue;
-				break;
-				case 4:
-				for( p = 0; p < shipL; p++ ) {
-					if(grid[startRowValue][startColValue+p] != 0 ) {
-						overlap = true;
-					}
-				}
-				endRowValue = startRowValue;
-				endColValue=startColValue+shipL;
-				break;
-				default:
-					System.out.println("wrong!");
-			} //switch
+                        overlap = findOverlap( shipL,
+                                               startRowValue,
+                                               startColValue,
+					       dir );
 
 			//fill in ships and make sure they
 			//don't overlap or go off the grid
@@ -182,46 +151,10 @@ public class BSGrid {
 				}
 
 				//check for ship overlap again.
-				switch( dir ) {
-					case 1:
-					for( p = 0; p < shipL; p++ ) {
-						if(grid[startRowValue+p][startColValue] != 0) {
-							overlap = true;
-						}
-					}
-					endRowValue=startRowValue+shipL;
-					endColValue = startColValue;
-					break;
-					case 2:
-					for( p = 0; p < shipL; p++ ) {
-						if(grid[startRowValue][startColValue-p] != 0 ) {
-							overlap = true;
-						}
-					}
-					endRowValue = startRowValue;
-					endColValue=startColValue-shipL;
-					break;
-					case 3:
-					for( p = 0; p < shipL; p++ ) {
-						if(grid[startRowValue-p][startColValue] != 0 ) {
-							overlap = true;
-						}
-					}
-					endRowValue=startRowValue-shipL;
-					endColValue = startColValue;
-					break;
-					case 4:
-					for( p = 0; p < shipL; p++ ) {
-						if(grid[startRowValue][startColValue+p] != 0 ) {
-							overlap = true;
-						}
-					}
-					endRowValue = startRowValue;
-					endColValue=startColValue+shipL;
-					break;
-					default:
-						System.out.println("wrong!");
-				} //switch
+				overlap = findOverlap( shipL,
+                                                       startRowValue,
+                                                       startColValue,
+						       dir );
 			}
 
 			//make the ship!
@@ -274,7 +207,54 @@ public class BSGrid {
         grid = newGrid;
     } //generateGrid()
 
-	/**
+    /**
+     *
+     *
+     */
+    private boolean findOverlap( int shipLength,
+                                 int startRowValue,
+                                 int startColValue,
+				 int dir ) {
+        boolean retVal = false;
+        int p = 0;
+
+        switch( dir ) {
+            case 1:
+                for( p = 0; p < shipLength; p++ ) {
+                    if(grid[startRowValue+p][startColValue] != 0) {
+                        retVal = true;
+                    }
+                }
+                break;
+            case 2:
+                for( p = 0; p < shipLength; p++ ) {
+                    if(grid[startRowValue][startColValue-p] != 0 ) {
+                        retVal = true;
+                    }
+                }
+                break;
+            case 3:
+                for( p = 0; p < shipLength; p++ ) {
+                    if(grid[startRowValue-p][startColValue] != 0 ) {
+                        retVal = true;
+                    }
+                }
+                break;
+            case 4:
+                for( p = 0; p < shipLength; p++ ) {
+                    if(grid[startRowValue][startColValue+p] != 0 ) {
+                        retVal = true;
+                    }
+                }
+                break;
+            default:
+                System.out.println("wrong!");
+        } //switch
+
+        return retVal;
+    }
+
+    /**
 	 * Creates a string that is the BattleShip grid in
 	 * human readable form.
 	 *
