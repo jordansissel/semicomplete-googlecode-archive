474ea5b8137a654089a507a9fb1077752df2b816
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-13 02:26:36 +0000

    added some basic stuff to connect method
    git-svn-id: http://semicomplete.googlecode.com/svn@517 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/TCPClientConnection.java
+++ trunk/datacomm/src/TCPClientConnection.java
@@ -1,26 +1,78 @@
 /*
  * TCPClientConnection.java
- * 
+ *
  * Version:
  *   $Id$
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.2  2004/01/13 02:26:36  tristan
+ *   added some basic stuff to connect method
+ *
  *   Revision 1.1  2004/01/13 01:34:41  tristan
  *   initial
  *
  */
 
+import java.net.InetAddress;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Manages a TCP based connection with the server.
- * 
+ *
  * @author tristan
  */
 public class TCPClientConnection extends ClientConnection {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
     /**
      * Creates a new TCPClientConnection
      */
-    public TCPClientConnection() {
+    public TCPClientConnection( InetAddress server, int port ) {
+        super( server, port );
+    }
 
+    /**
+     * Reads and sends messages to the server.
+     */
+    public void run() {
+        try {
+
+        } catch ( Exception e ) {
+            System.err.println( e.getMessage() );
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Starts a tcp connection with the server.
+     */
+    public void connect() {
+        try {
+            socket = new Socket( getServerHost(), getServerPort() );
+            out = new PrintWriter( socket.getOutputStream(), true );
+            in = new BufferedReader( new InputStreamReader(
+                                     socket.getInputStream() ) );
+        } catch ( Exception e ) {
+            System.err.println( e.getMessage() );
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Disconnects from the server.
+     */
+    public void disconnect() {
+        try {
+            socket.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getMessage() );
+            e.printStackTrace();
+        }
     }
 }   // TCPClientConnection
