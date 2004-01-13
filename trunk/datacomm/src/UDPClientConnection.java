23dc4189585474e5b910878c43dc99a16ac3a416
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-13 14:55:00 +0000

    started connect method.
    git-svn-id: http://semicomplete.googlecode.com/svn@524 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/UDPClientConnection.java
+++ trunk/datacomm/src/UDPClientConnection.java
@@ -6,6 +6,9 @@
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.4  2004/01/13 14:55:00  tristan
+ *   started connect method.
+ *
  *   Revision 1.3  2004/01/13 02:26:52  tristan
  *   stubed out run method.
  *
@@ -18,6 +21,8 @@
  */
 
 import java.net.InetAddress;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
 
 /**
  * Manages a UDP based connection with the server
@@ -25,11 +30,27 @@ import java.net.InetAddress;
  * @author tristan
  */
 public class UDPClientConnection extends ClientConnection {
+    // constants
+    private static final long RETRY_DELAY = 5 * 1000;
+    private static final int RETRY_COUNT = 3;
+    private static final int BUFFER_SIZE = 1024;
+    private static final int TIMEOUT = 30 * 1000;
+
+    // member variables
+    private DatagramSocket socket;
+
     /**
      * Creates a new UDPClientConnection
      */
     public UDPClientConnection( InetAddress server, int port ) {
         super( server, port );
+
+        try {
+            socket = new DatagramSocket();
+        } catch ( Exception e ) {
+            System.err.println( e.getMessage() );
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -37,7 +58,12 @@ public class UDPClientConnection extends ClientConnection {
      */
     public void run() {
         try {
-            
+            while ( isConnected() ) {
+                // send alive msg
+                // listen for response
+                // sleep delay time
+                sleep( RETRY_DELAY );
+            }
         } catch ( Exception e ) {
             System.err.println( e.getMessage() );
             e.printStackTrace();
@@ -47,14 +73,34 @@ public class UDPClientConnection extends ClientConnection {
     /**
      * Disconnects from the server.
      */
-    public void disconnect() {
-
+    public synchronized void disconnect() {
+        setConnected( false );
     }
 
     /**
      * Starts a UDP transaction with the server.
      */
-    public void connect() {
+    public synchronized boolean connect() {
+        boolean retVal = false;
 
+        // attempt to send a hello to the server
+        socket.connect( getServerHost(), getServerPort() );
+        byte[] buffer = new byte[ BUFFER_SIZE ];
+        DatagramPacket packet = new DatagramPacket( buffer, buffer.length );
+        try {
+            // TODO create special hello packet
+            socket.send( packet );
+            socket.setSoTimeout( TIMEOUT );
+            socket.receive( packet );
+            retVal = true;
+        } catch ( Exception e ) {
+            System.err.println( e.getMessage() );
+            e.printStackTrace();
+        }
+
+        // update status
+        setConnected( retVal );
+
+        return retVal;
     }
 }   // UDPClientConnection
