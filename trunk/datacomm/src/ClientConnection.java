dac37ab61df3c3303eaadc04422f16f340c16fed
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-13 01:53:23 +0000

    added a var
    git-svn-id: http://semicomplete.googlecode.com/svn@515 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/ClientConnection.java
+++ trunk/datacomm/src/ClientConnection.java
@@ -1,21 +1,28 @@
 /*
  * ClientConnection.java
- * 
+ *
  * Version:
  *   $Id$
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.2  2004/01/13 01:53:23  tristan
+ *   added a var
+ *
  *   Revision 1.1  2004/01/13 01:33:38  tristan
  *   initial
  *
  */
 
+import java.net.InetAddress;
+
 /**
  * Manages a client connection with the server.
- * 
+ *
  * @author tristan
  */
-public abstract class ClientConnection {
-    
+public abstract class ClientConnection extends Thread {
+    protected InetAddress serverHost;
+
+
 }   // ClientConnection
