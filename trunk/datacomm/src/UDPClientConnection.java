c5ddafea13f40cd4f04c9f06d18d93aafa96a676
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-13 02:26:52 +0000

    stubed out run method.
    git-svn-id: http://semicomplete.googlecode.com/svn@518 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/UDPClientConnection.java
+++ trunk/datacomm/src/UDPClientConnection.java
@@ -6,6 +6,9 @@
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.3  2004/01/13 02:26:52  tristan
+ *   stubed out run method.
+ *
  *   Revision 1.2  2004/01/13 01:36:54  tristan
  *   forgot something
  *
@@ -13,6 +16,8 @@
  *   initial
  *
  */
+
+import java.net.InetAddress;
 
 /**
  * Manages a UDP based connection with the server
@@ -23,7 +28,33 @@ public class UDPClientConnection extends ClientConnection {
     /**
      * Creates a new UDPClientConnection
      */
-    public UDPClientConnection() {
+    public UDPClientConnection( InetAddress server, int port ) {
+        super( server, port );
+    }
+
+    /**
+     * Reads and sends messages to the server.
+     */
+    public void run() {
+        try {
+            
+        } catch ( Exception e ) {
+            System.err.println( e.getMessage() );
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Disconnects from the server.
+     */
+    public void disconnect() {
+
+    }
+
+    /**
+     * Starts a UDP transaction with the server.
+     */
+    public void connect() {
 
     }
 }   // UDPClientConnection
