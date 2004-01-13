bbef153739c786667e1ef3a389fe68a9bc75281b
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-13 01:36:54 +0000

    forgot something
    git-svn-id: http://semicomplete.googlecode.com/svn@514 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/UDPClientConnection.java
+++ trunk/datacomm/src/UDPClientConnection.java
@@ -1,11 +1,14 @@
 /*
  * UDPClientConnection.java
- * 
+ *
  * Version:
  *   $Id$
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.2  2004/01/13 01:36:54  tristan
+ *   forgot something
+ *
  *   Revision 1.1  2004/01/13 01:35:15  tristan
  *   initial
  *
@@ -13,10 +16,10 @@
 
 /**
  * Manages a UDP based connection with the server
- * 
+ *
  * @author tristan
  */
-public class UDPClientConnection {
+public class UDPClientConnection extends ClientConnection {
     /**
      * Creates a new UDPClientConnection
      */
