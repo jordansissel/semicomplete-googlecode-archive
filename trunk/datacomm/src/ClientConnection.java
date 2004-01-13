137db456488c22434d7fe6aa5225f6e806d5f162
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-13 02:27:13 +0000

    added accessors and mutators.
    git-svn-id: http://semicomplete.googlecode.com/svn@519 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/ClientConnection.java
+++ trunk/datacomm/src/ClientConnection.java
@@ -6,6 +6,9 @@
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.3  2004/01/13 02:27:13  tristan
+ *   added accessors and mutators.
+ *
  *   Revision 1.2  2004/01/13 01:53:23  tristan
  *   added a var
  *
@@ -23,6 +26,58 @@ import java.net.InetAddress;
  */
 public abstract class ClientConnection extends Thread {
     protected InetAddress serverHost;
+    protected int serverPort;
 
+    /**
+     * Initializes the server's inet address.
+     * @param serverHost The server's inet address.
+     */
+    public ClientConnection( InetAddress serverHost, int serverPort ) {
+        this.serverHost = serverHost;
+        this.serverPort = serverPort;
+    }
+
+    /**
+     * Returns the server's inet address.
+     * @return The current server's inet address.
+     */
+    public InetAddress getServerHost() {
+        return serverHost;
+    }
+
+    /**
+     * Sets the server's inet address.
+     * @param serverHost The new server inet address.
+     */
+    public void setServerHost( InetAddress serverHost ) {
+        this.serverHost = serverHost;
+    }
+
+    /**
+     * Returns the server's port.
+     * @return The server's port.
+     */
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    /**
+     * Changes the server's port number.
+     * @param serverPort The new server port number.
+     */
+    public void setServerPort( int serverPort ) {
+        this.serverPort = serverPort;
+    }
+
+    /**
+     * Disconnects the connection from the server
+     */
+    public void finalize() {
+        disconnect();
+    }
+
+    // abstract methods
+    public abstract void connect();
+    public abstract void disconnect();
 
 }   // ClientConnection
