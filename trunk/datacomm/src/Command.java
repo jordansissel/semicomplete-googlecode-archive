5a3548643a4889d6bbe35cae875f84c96debae82
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-14 04:36:04 +0000

    initial
    git-svn-id: http://semicomplete.googlecode.com/svn@530 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/datacomm/src/Command.java
@@ -0,0 +1,26 @@
+/*
+ * Command.java
+ * 
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/14 04:36:04  tristan
+ *   initial
+ *
+ */
+
+/**
+ * Command sent to a client from the server.
+ * 
+ * @author tristan
+ */
+public class Command {
+    /**
+     * Creates a new Command
+     */
+    public Command() {
+
+    }
+}   // Command
---
+++ trunk/datacomm/src/Response.java
@@ -0,0 +1,26 @@
+/*
+ * Response.java
+ * 
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/14 04:35:27  tristan
+ *   initial
+ *
+ */
+
+/**
+ * Response to a command query.
+ * 
+ * @author tristan
+ */
+public class Response {
+    /**
+     * Creates a new Response
+     */
+    public Response() {
+
+    }
+}   // Response
