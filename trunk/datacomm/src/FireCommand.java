97e1fb74bd9f4ba022e7544454c22db772cb699c
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-18 23:54:53 +0000

    - Stub written for FireCommand
    git-svn-id: http://semicomplete.googlecode.com/svn@544 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/datacomm/src/FireCommand.java
@@ -0,0 +1,22 @@
+/*
+ * FireCommand.java
+ *
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/18 23:54:53  psionic
+ *   - Stub written for FireCommand
+ *
+ *
+ */
+
+import java.util.List;
+
+public class FireCommand extends Command {
+
+	/* Syntax definition for arguments */
+	public String[] syntax = {"%c", "%i"};
+
+}
