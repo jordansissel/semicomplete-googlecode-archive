ce5215402f14e29911fbc99c9c2776f06553daec
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-18 22:54:15 +0000

    initial
    git-svn-id: http://semicomplete.googlecode.com/svn@535 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/datacomm/src/BattleshipClient.java
@@ -0,0 +1,26 @@
+/*
+ * BattleshipClient.java
+ * 
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/18 22:54:15  tristan
+ *   initial
+ *
+ */
+
+/**
+ * Starts up the battleship client
+ *
+ * @author tristan
+ */
+public class BattleshipClient {
+    /**
+     * Creates a new BattleshipClient
+     */
+    public BattleshipClient() {
+
+    }
+}   // BattleshipClient
