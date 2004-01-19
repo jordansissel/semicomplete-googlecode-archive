3cc52e525256b9f501fa1b397a4d15445c6e1727
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-19 04:54:07 +0000

    - Added generic framework for syntax checking
    git-svn-id: http://semicomplete.googlecode.com/svn@567 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/FireCommand.java
+++ trunk/datacomm/src/FireCommand.java
@@ -6,6 +6,9 @@
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.2  2004/01/19 04:54:07  psionic
+ *   - Added generic framework for syntax checking
+ *
  *   Revision 1.1  2004/01/18 23:54:53  psionic
  *   - Stub written for FireCommand
  *
@@ -16,7 +19,37 @@ import java.util.List;
 
 public class FireCommand extends Command {
 
-	/* Syntax definition for arguments */
-	public String[] syntax = {"%c", "%i"};
+	public FireCommand() {
+		super("fire");
+	}
+
+	public FireCommand(List args) {
+		super("fire", args);
+	}
+
+	public FireCommand(String args) throws InvalidCommandArgumentsException {
+		super("fire");
+		this.args = this.parseArgs(args);
+	}
+
+	private boolean validateArguments(List args) 
+	        throws InvalidCommandArgumentsException {
+		System.err.println("Validating args: " + args);
+
+		// First Argument
+		String arg = (String) args.get(0);
+		if ( (arg.length() > 1) || (!arg.matches("[a-jA-J]")) ) {
+			return false;
+		}
+
+		// Second Argument
+		try {
+			Integer i = new Integer((String)args.get(1));
+		} catch (Exception e) {
+			return false;
+		}
+
+		return true;
+	}
 
 }
