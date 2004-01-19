a211b393a8c2115d74d888496489b6fe9174cbb1
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-19 04:01:53 +0000

    implemented run method.
    git-svn-id: http://semicomplete.googlecode.com/svn@562 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/TextClient.java
+++ trunk/datacomm/src/TextClient.java
@@ -6,6 +6,9 @@
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.3  2004/01/19 04:01:53  tristan
+ *   implemented run method.
+ *
  *   Revision 1.2  2004/01/19 02:35:25  tristan
  *   moved stuff into text client
  *
@@ -21,7 +24,10 @@
  */
 public class TextClient extends BSClient {
     /**
-     * Creates a new TextClient
+     * Initializes the text client.
+     * @param connection The client connection.
+     * @param yourName This player's name.
+     * @param otherName The opponent's name
      */
     public TextClient( ClientConnection connection,
                        String yourName,
@@ -34,17 +40,86 @@ public class TextClient extends BSClient {
      */
     public void run() {
         if ( getConnection().connect() ) {
-            // send a start game message
+            // initialize game
+            startGame();
+            findOpponent();
 
-            // block until server says opponent found
-
-
-            while ( getConnection().isConnected() ) {
-
-                // talk to server
+            // start game loop
+            while ( !isGameOver() && getConnection().isConnected() ) {
+                if ( !isClientTurn() ) {
+                    waitForTurn();
+                } else {
+                    doTurn();
+                }
             }
+
+            // print out who won etc
+            printGameFinish();
         } else {
             System.out.println( "Failing to connect to server" );
         }
+    }
+
+    /**
+     * Does the user's turn
+     */
+    public void doTurn() {
+        String input = null;
+        boolean validInput = false;
+        char row = 'a';
+        int col = 1;
+        String errorMsg = "Input must be [A-J] [1-10]";
+
+        // get the row and column
+        do {
+            input = StringInput.getInput( "Input row and column: " );
+            String[] vars = input.split( "\\s" );
+
+            // check length
+            if ( vars.length != 2 ) {
+                System.out.println( errorMsg );
+                continue;
+            }
+
+            // check row
+            if ( vars[ 0 ].length() > 1 ) {
+                System.out.println( errorMsg );
+                continue;
+            }
+
+            // make sure it's between a-j
+            row = vars[ 0 ].toLowerCase().charAt( 0 );
+            if ( row < 'a' || row > 'j' ) {
+                System.out.println( errorMsg );
+                continue;
+            }
+
+            // parse column
+            try {
+                col = Integer.parseInt( vars[ 1 ] );
+            } catch ( NumberFormatException e ) {
+                System.out.println( errorMsg );
+                continue;
+            }
+
+            // make sure the number is between 1 and 10
+            if ( col < 1 || col > 10 ) {
+                System.out.println( errorMsg );
+                continue;
+            }
+
+            // we actually parsed everything
+            validInput = true;
+        } while ( !validInput );
+
+        // send a message to the server
+        sendCall( row, col );
+    }
+
+    /**
+     * Prints who won, etc.
+     */
+    public void printGameFinish() {
+
     }
 }   // TextClient
