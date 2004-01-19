5869acab73b53054b5eada6ff38248acff18da44
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-19 20:40:58 +0000

    refactored to a new file name.
    git-svn-id: http://semicomplete.googlecode.com/svn@587 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/datacomm/src/Server.java
@@ -0,0 +1,32 @@
+/*
+ * Server.java
+ * 
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/19 20:40:58  tristan
+ *   refactored to a new file name.
+ *
+ *   Revision 1.2  2004/01/18 23:49:26  tristan
+ *   updated comments
+ *
+ *   Revision 1.1  2004/01/18 23:39:05  tristan
+ *   initial
+ *
+ */
+
+/**
+ * Manages a server's connection with the client
+ *
+ * @author tristan
+ */
+public abstract class Server {
+    /**
+     * Creates a new Server
+     */
+    public Server() {
+
+    }
+}   // Server
