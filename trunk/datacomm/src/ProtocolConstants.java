79fa1aa047db230258cfd977ce25e056efd28dbe
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-19 23:21:16 +0000

    added player found constant
    git-svn-id: http://semicomplete.googlecode.com/svn@604 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/ProtocolConstants.java
+++ trunk/datacomm/src/ProtocolConstants.java
@@ -6,6 +6,9 @@
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.2  2004/01/19 23:21:16  tristan
+ *   added player found constant
+ *
  *   Revision 1.1  2004/01/19 20:58:21  tristan
  *   started class
  *
@@ -17,10 +20,15 @@
  * @author Tristan O'Tierney
  */
 public interface ProtocolConstants {
+    // 100s: startup connection constants
     public static final String HELLO = "HELLO";
     public static final int HELLO_RESPONSE = 101;
 
     public static final String STARTGAME = "STARTGAME";
     public static final int STARTGAME_RESPONSE = 102;
+
+    public static final int PLAYERFOUND = 103;
+
+    
 }   // ProtocolConstants
 
