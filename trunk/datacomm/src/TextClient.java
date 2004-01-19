aadda2068a6b7d0f92eb18412f4b813a98f3ad0e
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-19 02:35:25 +0000

    moved stuff into text client
    git-svn-id: http://semicomplete.googlecode.com/svn@559 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/TextClient.java
+++ trunk/datacomm/src/TextClient.java
@@ -6,6 +6,9 @@
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.2  2004/01/19 02:35:25  tristan
+ *   moved stuff into text client
+ *
  *   Revision 1.1  2004/01/19 02:21:48  tristan
  *   initial
  *
@@ -16,11 +19,32 @@
  *  
  * @author tristan
  */
-public class TextClient {
+public class TextClient extends BSClient {
     /**
      * Creates a new TextClient
      */
-    public TextClient() {
+    public TextClient( ClientConnection connection,
+                       String yourName,
+                       String otherName ) {
+        super( connection, yourName, otherName );
+    }
 
+    /**
+     * Runs the text client by talking to the server
+     */
+    public void run() {
+        if ( getConnection().connect() ) {
+            // send a start game message
+
+            // block until server says opponent found
+
+
+            while ( getConnection().isConnected() ) {
+
+                // talk to server
+            }
+        } else {
+            System.out.println( "Failing to connect to server" );
+        }
     }
 }   // TextClient
