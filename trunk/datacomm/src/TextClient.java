2d3e9ab93b2ddb0318effd78ba19c968a23479c9
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-19 02:21:48 +0000

    initial
    git-svn-id: http://semicomplete.googlecode.com/svn@553 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/datacomm/src/TextClient.java
@@ -0,0 +1,26 @@
+/*
+ * TextClient.java
+ * 
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/19 02:21:48  tristan
+ *   initial
+ *
+ */
+
+/**
+ * Text based Battle ship client
+ *  
+ * @author tristan
+ */
+public class TextClient {
+    /**
+     * Creates a new TextClient
+     */
+    public TextClient() {
+
+    }
+}   // TextClient
