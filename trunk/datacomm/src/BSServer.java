039d150358355a028dd458762b95101bde81c5b5
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-20 06:54:32 +0000

    initial
    git-svn-id: http://semicomplete.googlecode.com/svn@637 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/datacomm/src/BSServer.java
@@ -0,0 +1,28 @@
+/*
+ * BSServer.java
+ * 
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/20 06:54:32  tristan
+ *   initial
+ *
+ */
+
+/**
+ * Manages and listens for clients connecting.
+ *
+ * @author Tristan O'Tierney
+ * @author Jordan Sissel
+ * @author Nick Johnson
+ */
+public class BSServer {
+    /**
+     * Creates a new BSServer
+     */
+    public BSServer() {
+
+    }
+}   // BSServer
