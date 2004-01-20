12a4d63c6daaa36e9763e0aecf43a46030c0778a
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-20 08:29:04 +0000

    set up basic layout.
    git-svn-id: http://semicomplete.googlecode.com/svn@644 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/datacomm/src/BSUDPServer.java
+++ trunk/datacomm/src/BSUDPServer.java
@@ -6,10 +6,16 @@
  *
  * Revisions:
  *   $Log$
+ *   Revision 1.2  2004/01/20 08:29:04  tristan
+ *   set up basic layout.
+ *
  *   Revision 1.1  2004/01/20 07:04:17  tristan
  *   initial
  *
  */
+
+import java.net.InetAddress;
+import java.net.DatagramSocket;
 
 /**
  * Manages UDP connections for a battle ship server.
@@ -18,11 +24,31 @@
  * @author Jordan Sissel
  * @author Nick Johnson
  */
-public class BSUDPServer {
-    /**
-     * Creates a new BSUDPServer
-     */
-    public BSUDPServer() {
+public class BSUDPServer extends BSProtocolServer {
+    private DatagramSocket socket;
 
+    /**
+     * Starts the udp server and binds to a host/port.
+     * @param server The main parent server.
+     * @param host The host to bind to.
+     * @param port The port to bind to.
+     */
+    public BSUDPServer( BSServer server, InetAddress host, int port ) {
+        super( server, host, port );
+
+        try {
+            socket = new DatagramSocket( port, host );
+        } catch ( Exception e ) {
+            shutdown();
+        }
+    }
+
+    /**
+     * Starts the event loop for udp listening.
+     */
+    public void run() {
+        while( isRunning() ) {
+            // listen for connections.
+        }
     }
 }   // BSUDPServer
