54cf8dd0d1643c8f1bc1d89180e44ddc2d68cda0
tristan <tristan@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-01-20 07:04:17 +0000

    initial
    git-svn-id: http://semicomplete.googlecode.com/svn@638 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/datacomm/src/BSTCPServer.java
@@ -0,0 +1,28 @@
+/*
+ * BSTCPServer.java
+ * 
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/20 07:03:17  tristan
+ *   initial
+ *
+ */
+
+/**
+ * Manages the TCP connections for a battleship server.
+ *
+ * @author Tristan O'Tierney
+ * @author Jordan Sissel
+ * @author Nick Johnson
+ */
+public class BSTCPServer extends Thread {
+    /**
+     * Creates a new BSTCPServer
+     */
+    public BSTCPServer() {
+
+    }
+}   // BSTCPServer
---
+++ trunk/datacomm/src/BSUDPServer.java
@@ -0,0 +1,28 @@
+/*
+ * BSUDPServer.java
+ * 
+ * Version:
+ *   $Id$
+ *
+ * Revisions:
+ *   $Log$
+ *   Revision 1.1  2004/01/20 07:04:17  tristan
+ *   initial
+ *
+ */
+
+/**
+ * Manages UDP connections for a battle ship server.
+ *
+ * @author Tristan O'Tierney
+ * @author Jordan Sissel
+ * @author Nick Johnson
+ */
+public class BSUDPServer {
+    /**
+     * Creates a new BSUDPServer
+     */
+    public BSUDPServer() {
+
+    }
+}   // BSUDPServer
