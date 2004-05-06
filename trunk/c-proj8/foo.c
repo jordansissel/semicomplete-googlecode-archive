fb25d794bc79840b1c43fb2adf6967e3604143d3
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-05-06 20:53:01 +0000

    *** empty log message ***
    git-svn-id: http://semicomplete.googlecode.com/svn@717 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/c-proj8/foo.c
+++ trunk/c-proj8/foo.c
@@ -44,32 +44,3 @@ char **do_opts(int argc, char **argv, char *control,
 
 	return pt;
 }
-
-void _do_opt(int ch, char *value) {
-	printf("do_opt('%c', ", ch);
-
-	if (value != NULL)
-		printf("\"%s\"", value);
-	else 
-		printf("0");
-
-	printf(")\n");
-}
-
-void _illegal_opt(int ch) {
-	printf("illegal_opt('%c')\n", ch);
-}
-
-int main(int argc, char **argv) {
-	char **f;
-	f = do_opts(argc, argv, "abcdef", _do_opt, _illegal_opt);
-
-	printf("Remaining: ");
-	while (*f != NULL) {
-		printf("%s ", *f);
-		*f++;
-	}
-
-	printf("\n");
-}
-
