afe4c1973962174e6edb094955f49c4a9cefae70
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-06-04 22:09:05 +0000

    - Added $state->{"away_responding"}->{"$from"}, which gets set to 1 in   event_im_in if you are away. This is used by command_msg to determine if   you are away or not. I should do this better :(
    git-svn-id: http://semicomplete.googlecode.com/svn@753 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/tic/Tic/Events.pm
+++ trunk/tic/Tic/Events.pm
@@ -165,6 +165,7 @@ sub event_im_in {
 	if (defined($state->{"away"})) {
 		return if (defined($state->{"away_respond"}->{$from}) && 
 					  $state->{"away_respond"}->{$from} + 600 > time());
+		$state->{"away_responding"}->{"$from"} = 1;
 		command_msg($state, '"' . $from .'" ' . $state->{"away"});
 		$state->{"away_respond"}->{$from} = time();
 	}
