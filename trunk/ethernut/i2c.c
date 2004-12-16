df34ba36349ccf2446dc54f3ee96d05d6d799179
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-12-16 21:19:31 +0000

    - Initial commit
    git-svn-id: http://semicomplete.googlecode.com/svn@868 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ trunk/ethernut/i2c.c
@@ -0,0 +1,11 @@
+/* 
+ * i2c.c
+ *
+ * $Id$
+ */
+
+#include "i2c.h"
+
+void i2c_init() {
+	log(1, "i2c_init()");
+}
---
+++ trunk/ethernut/i2c.h
@@ -0,0 +1,12 @@
+/* 
+ * i2c.h
+ *
+ * $Id$
+ */
+
+#ifndef I2C_H
+#define I2C_h
+
+void i2c_init();
+
+#endif
