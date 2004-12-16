65e53dfee522046d280088f1fe53109c79d4c164
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-12-16 19:11:43 +0000

    - Now with logging goodness
    git-svn-id: http://semicomplete.googlecode.com/svn@863 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/ethernut/busmaster.c
+++ trunk/ethernut/busmaster.c
@@ -5,14 +5,17 @@
  */
 
 #include "busmaster.h"
+#include "log.h"
 
 busmaster_t *busmaster;
 
+/* Initialize the busmaster struct */
 void busmaster_init() {
+	log(1, "busmaster_init()");
+
 	busmaster = (busmaster_t *) malloc(sizeof(busmaster_t));
 
 	busmaster->addr = 0;
 	busmaster->slaves = NULL;
 	busmaster->slavecount = 0;
 }
-
