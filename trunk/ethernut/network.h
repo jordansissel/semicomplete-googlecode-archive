7a268b00d485bb8d1ee7dea4c4fc345866e204e0
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-12-24 08:15:32 +0000

    - Added nut_t *nuts and nut_count - Added DISCOVERY_INTERVAL - Added network_addnut() - probably wont use this
    git-svn-id: http://semicomplete.googlecode.com/svn@893 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/ethernut/network.h
+++ trunk/ethernut/network.h
@@ -9,6 +9,17 @@
 
 #define DISCOVERY_PORT 3000
 #define DISCOVERY_MESSAGE "Hello!"
+#define DISCOVERY_INTERVAL 1
+
+typedef struct nut {
+	//unsigned int ip;
+	struct in_addr ip;
+	time_t lastseen;
+} nut_t;
+
+/* List of known nuts */
+nut_t *nuts = NULL;
+unsigned int nut_count = 0;
 
 /* network_init
  * Initialize the network bits. This includes running DHCP and setting up a
@@ -26,5 +37,8 @@ void network_start_thread();
 
 /* The actual network thread */
 void network_thread(void *args);
+
+/* Register a new EtherNut that was found */
+void network_addnut(unsigned int ip);
 
 #endif
