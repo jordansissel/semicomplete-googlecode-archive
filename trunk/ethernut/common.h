f13a0d1ebfa982cfa49895cf4d003f8ef05fed48
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-12-25 20:15:57 +0000

    - Added THREAD_CREATE and THREAD_EXIT for NutOS and POSIX - Added THREAD() function-declaration macro for POSIX, NutOS provides   this. - Some slightlogging for MUTEX_LOCK and MUTEX_UNLOCK calls - for POSIX, MUTEX is no longer a #define, it's a typedef like it is in   NutOS
    git-svn-id: http://semicomplete.googlecode.com/svn@904 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/ethernut/common.h
+++ trunk/ethernut/common.h
@@ -16,13 +16,23 @@
 #define MUTEX_LOCK(m) NutMutexLock(m)
 #define MUTEX_UNLOCK(m) NutMutexUnlock(m)
 #define MUTEX_DESTROY(m) NutMutexDestroy(m)
+
+#define THREAD_CREATE(name, func, arg, stacksize) NutThreadCreate(name, func, arg, stacksize)
+#define THREAD_EXIT() NutThreadExit()
+
 #else
+
+/* POSIX-style threads and mutexes (Linux, FreeBSD, Solaris) */
+
 #define MUTEX_INIT(m) pthread_mutex_init(m, NULL)
-#define MUTEX_LOCK(m) pthread_mutex_lock(m)
-#define MUTEX_UNLOCK(m) pthread_mutex_unlock(m)
+#define MUTEX_LOCK(m) log(10, "Locking mutex..."), pthread_mutex_lock(m)
+#define MUTEX_UNLOCK(m) log(10, "Unlocking mutex..."), pthread_mutex_unlock(m)
 #define MUTEX_DESTROY(m) pthread_mutex_destroy(m)
 
-#define MUTEX pthread_mutex_t
+#define THREAD_CREATE(thread, attr, func, args) pthread_create(thread, attr, func, args)
+#define THREAD(func, args) void func(void *args)
+#define THREAD_EXIT() pthread_exit(NULL)
+typedef pthread_mutex_t MUTEX;
 #endif
 
 #endif
