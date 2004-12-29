5e5d92a3dbe15148d6ab05945a3058439994b860
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-12-29 09:09:33 +0000

    - Removed DISCOVERY_MESSAGE and DISCOVERY_ACK - Added a compile-time check ensuring DISCOVERY_INTERVAL >   DISCOVERY_MAXWAIT. Warnings are issued if this is false. - Added a connection_t structure. These are handed off to   new network_tcphandler threads. - More documentation goodness.
    git-svn-id: http://semicomplete.googlecode.com/svn@922 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/ethernut/network.h
+++ trunk/ethernut/network.h
@@ -11,8 +11,6 @@
 #include "common.h"
 
 #define DISCOVERY_PORT 3000
-#define DISCOVERY_MESSAGE "DISCOVERY"
-#define DISCOVERY_ACK "ACK"
 
 #define PACKETTYPE_DATA            ((char)0x01)
 #define PACKETTYPE_ERROR_BROADCAST ((char)0x02)
@@ -24,18 +22,33 @@
 #define PACKETTYPE_SOURCE_QUENCH   ((char)0x08)
 #define PACKETTYPE_REBOOT          ((char)0x09)
 
-
 /* How long to wait before repinging each nut */
 #define DISCOVERY_INTERVAL 5 
 
 /* How long to wait before not having gotten a response from a ping */
-#define DISCOVERY_MAXWAIT 5
+#define DISCOVERY_MAXWAIT 10
 
+#ifndef IGNORE_INTERVAL_ERROR
+#	if DISCOVERY_INTERVAL >= DISCOVERY_MAXWAIT
+#		warning "DISCOVERY_INTERVAL must be less than DISCOVERY_MAXWAIT"
+#     warning "Recompile with -DIGNORE_INTERVAL_ERROR to ignore this"
+//		error "DISCOVERY_INTERVAL must be less than DISCOVERY_MAXWAIT. Recompile with -DIGNORE_INTERVAL_ERROR to ignore this."
+#	endif
+#endif
+
+/* A nut (Bus Master) */
 typedef struct nut {
 	//unsigned int ip;
 	struct in_addr ip;
 	time_t lastseen;
 } nut_t;
+
+/* A new tcp connection, passed off to the network_tcphandler threads */
+typedef struct connection {
+	int fd;
+	struct sockaddr_in src;
+} connection_t;
+
 
 /* network_init
  * Initialize the network bits. This includes running DHCP and setting up a
@@ -49,18 +62,26 @@ int network_send_discover();
 /* Start up the network thread */
 void network_start_thread();
 
-/* The actual network thread */
-THREAD(network_thread, args);
-//void network_thread(void *args);
+/* The ethernut (busmaster) discovery thread */
+THREAD(network_discoverythread, args);
 
 /* The pinger thread */
 THREAD(network_pingthread, args);
-//void network_pingthread(void *args);
+
+/* TCP Communication thread */
+THREAD(network_communicationthread, args);
+
+/* New connection handler, created on a per-connection basis */
+THREAD(network_tcphandler, args);
 
 /* Register a new EtherNut that was found */
 void network_addnut(struct in_addr ip);
 
 /* Remove a nut, by index */
 void network_removenut(int index);
+
+/* Broadcast a message over tcp 
+ * This will only send messages to known busmasters/ethernuts */
+void network_tcpbroadcast(char *message, int bytes);
 
 #endif
