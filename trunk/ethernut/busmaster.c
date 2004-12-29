6ecd85e885b768abd1c5ef9038d4b84c56d3a89f
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-12-29 08:57:47 +0000

    - Don't malloc busmaster in busmaster_init, don't need to. - Added busmastercount global.
    git-svn-id: http://semicomplete.googlecode.com/svn@914 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/ethernut/busmaster.c
+++ trunk/ethernut/busmaster.c
@@ -9,16 +9,16 @@
 #include "busmaster.h"
 #include "log.h"
 
-
-busmaster_t *busmaster;
+busmaster_t *busmaster = NULL;
+int busmastercount = 0;
 
 /* Initialize the busmaster struct */
 void busmaster_init() {
 	log(1, "busmaster_init()");
 
-	busmaster = (busmaster_t *) malloc(sizeof(busmaster_t));
+	//busmaster = (busmaster_t *) malloc(sizeof(busmaster_t));
 
-	busmaster->addr = 0;
-	busmaster->slaves = NULL;
-	busmaster->slavecount = 0;
+	//busmaster->addr = 0;
+	//busmaster->slaves = NULL;
+	//busmaster->slavecount = 0;
 }
