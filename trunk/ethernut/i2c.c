7bc2a5aa0317dbdca68a5a488af80100b0989ef9
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-12-29 09:03:17 +0000

    - Added twif include if not ETHERNUT build - i2c_init now calls TwInit() and other goodies - wrote i2c_findslaves - tries to find i2c slaves on the i2c network - wrote i2c_addnode - adds new nodes to the nodes array
    git-svn-id: http://semicomplete.googlecode.com/svn@918 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/ethernut/i2c.c
+++ trunk/ethernut/i2c.c
@@ -4,9 +4,59 @@
  * $Id$
  */
 
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef ETHERNUT
+#include <dev/twif.h>
+#else
+#include "twif.h"
+#endif
+
 #include "i2c.h"
 #include "log.h"
 
+static i2cnode_t *nodes = NULL;
+static int nodecount = 0;
+
+/* The speed of our happy I2C bus */
+static const int i2c_speed = 2400;
+
+static u_char myaddr = 0; /* ??? What address do I use? */
 void i2c_init() {
 	log(1, "i2c_init()");
+
+	/* Initialize TWI/I2C interface */
+	TwInit(myaddr);
+
+	/* Set the bus speed to the correct speed.... */
+	TwIOCtl(TWI_SETSPEED, (void *)&i2c_speed);
+
+	i2c_findslaves();
+
+	//i2c_start_threads();
 }
+
+void i2c_findslaves() {
+	byte slaveaddr = TWSLA_MIN; /* Lowest slave address */
+	byte data[2];
+	byte recvdata[2];
+	memset(data, '\0', 2);
+
+	data[0] = I2C_DATA_PING;
+
+	for (; slaveaddr <= TWSLA_MAX; slaveaddr++) {
+		TwMasterTransact(slaveaddr, (void *)data, I2C_DATA_LENGTH, 
+							  (void *)recvdata, I2C_DATA_LENGTH, 0);
+	}
+
+}
+
+void i2c_addnode(byte addr) {
+	log(10, "Adding new I2C node: 0x%02x", addr);
+	nodecount++;
+	nodes = (i2cnode_t *) realloc(sizeof(i2cnode_t) * nodecount);
+	nodes[nodecount - 1].addr = addr;
+	nodes[nodecount - 1].status = 0;
+}
+
