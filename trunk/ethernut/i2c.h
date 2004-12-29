f2e7c5544e2e80a224fb6afaba585463ff517a6e
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-12-29 09:01:11 +0000

    - Added inclusion of "twif.h" for non-ethernut(AVRGCC) builds. - i2c_init now calls TwInit() and TwIOCtl(SETSPEED,...) - wrote i2c_findslaves, runs through list of valid slave addresses and   tries to write data to them, probably does this incorrectly. - wrote i2c_addnode. Adds a new i2cnode_t to nodes
    git-svn-id: http://semicomplete.googlecode.com/svn@917 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/ethernut/i2c.h
+++ trunk/ethernut/i2c.h
@@ -7,6 +7,23 @@
 #ifndef I2C_H
 #define I2C_h
 
+/* So that things are somewhat more readable */
+typedef u_char byte;
+
+typedef struct i2cnode {
+	byte addr; /* The address of the node */
+	byte status; /* Bit array....ish thing */
+} i2cnode_t;
+	
+#define I2C_DROPBIT 001
+#define I2C_QUENCHBIT 002
+#define I2C_RESPOND 004
+
+#define I2C_DATA_PING ((u_char)0x00)
+#define I2C_DATA_LENGTH 2
+
 void i2c_init();
+void i2c_addnode(byte addr);
+void i2c_findslaves();
 
 #endif
