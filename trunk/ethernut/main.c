e80c935f893614d54538eee50827bbfbc93f6fd5
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2004-12-29 09:04:14 +0000

    - Lots of dirty code that I thought would make things cleanup easier.   I was wrong because I'm bad at life. This code isn't used.   Checking it in anyway for history.
    git-svn-id: http://semicomplete.googlecode.com/svn@920 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/ethernut/main.c
+++ trunk/ethernut/main.c
@@ -7,6 +7,12 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <signal.h>
+
+#define _GNU_SOURCE
+#include <string.h>
 
 #include "busmaster.h"
 #include "network.h"
@@ -15,14 +21,49 @@
 
 /* from busmaster.c */
 extern busmaster_t *busmaster;
+int done = 0;
+
+int threadcount;
+pthread_t *threads;
+pthread_t mainthread;
+
+void addthread(pthread_t thread) {
+	threadcount++;
+	threads = realloc(threads, sizeof(pthread_t) * threadcount);
+	threads[threadcount - 1] = thread;
+}
+
+void cleanup(int sig) { 
+	int i = 0;
+
+	done = 1; 
+
+	if (pthread_self() != mainthread)
+		return; 
+
+	log(0, "%s received - Cleaning up...", strsignal(sig));
+
+	for (; i < threadcount; i++) {
+		log(0, "Waiting on thread #%d to finish", i);
+		pthread_join(threads[i], NULL);
+	}
+
+	exit(0);
+}
 
 int main(int argc, char **argv) {
+
+	mainthread = pthread_self();
 
 	set_log_level(100);
 
 	busmaster_init();
 	network_init();
 	i2c_init();
+
+	//signal(SIGINT, cleanup);
+	//signal(SIGTERM, cleanup);
+	//signal(SIGHUP, cleanup);
 
 	for (;;) {
 		//log(0, "main says hi");
