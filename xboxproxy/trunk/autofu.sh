2c89a34543912b8bb00606b9763261397cfda1e3
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2005-05-12 15:32:19 +0000

    - Fixes!
    git-svn-id: http://semicomplete.googlecode.com/svn@328 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- xboxproxy/trunk/autofu.sh
+++ xboxproxy/trunk/autofu.sh
@@ -1,6 +1,8 @@
 set -x
 set -e
-aclocal15 || aclocal
-autoheader259  || autoheader
+touch NEWS README AUTHORS ChangeLog depcomp
+aclocal15 || aclocal19 || aclocal
+autoheader259 || autoheader
 autoconf259 || autoconf
-automake15 -a Makefile || automake15 -a Makefile
+automake15 -a Makefile || automake19 -a Makefile || automake -a Makefile
+
