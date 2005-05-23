4a1d8b33b158021683871e729a155c57b0e5f951
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2005-05-23 15:43:02 +0000

    *** empty log message ***
    git-svn-id: http://semicomplete.googlecode.com/svn@1106 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/mpich/hello/hello.c
+++ trunk/mpich/hello/hello.c
@@ -10,8 +10,7 @@ int main(int argc, char **argv) {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	printf("Rank: %d\n", rank);
-	system("uname -a");
+	printf("Hello world (Rank: %d)\n", rank);
 
 	MPI_Finalize();
 	return 0;
