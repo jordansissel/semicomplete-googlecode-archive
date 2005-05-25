34685abbcd7653a382e38de44dedaad9edbf45a8
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2005-05-25 19:08:42 +0000

    *** empty log message ***
    git-svn-id: http://semicomplete.googlecode.com/svn@1107 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/mpich/hello/hello.c
+++ trunk/mpich/hello/hello.c
@@ -6,11 +6,14 @@
 
 int main(int argc, char **argv) {
 	int rank;
+	char host[150];
+	int namelen;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Get_processor_name(host,&namelen);
 
-	printf("Hello world (Rank: %d)\n", rank);
+	printf("Hello world (Rank: %d / Host: %s)\n", rank, host);
 
 	MPI_Finalize();
 	return 0;
