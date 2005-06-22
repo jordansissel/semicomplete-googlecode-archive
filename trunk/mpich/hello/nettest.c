61e99129b859a5e545fcfa167862d9053713f502
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2005-06-22 15:17:39 +0000

    - Send single integers at a time. - Slaves now expect a response from the master
    git-svn-id: http://semicomplete.googlecode.com/svn@1123 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/mpich/hello/nettest.c
+++ trunk/mpich/hello/nettest.c
@@ -14,13 +14,16 @@ char myhost[150];
 int myrank;
 
 #define SLEEP 1
-#define ITERATIONS 100000
+#define ITERATIONS 10
 
 void debug(const char *format, ...) {
 	char str[1024];
 	char msg[1024];
 	char host[150];
 	va_list args;
+
+	/* Don't debug right now */
+	return;
 
 	va_start(args,format);
 	vsnprintf(msg, 1024, format, args);
@@ -59,22 +62,40 @@ void master() {
 	MPI_Comm_size(MPI_COMM_WORLD, &nodes);
 	debug("EXAMPLE VALUE: %d", 15);
 
-	for (count = 0; count < ITERATIONS; count++) {
+	//for (count = 0; count < ITERATIONS; count++) {
+	for (;;) {
 		int x = 0;
 		MPI_Status status;
 		debug("Waiting for data...");
 		for (x = 1; x < nodes; x++) {
 			MPI_Recv(&result, 1, MPI_INT, x, 1, MPI_COMM_WORLD, &status);
+			result = 0;
+			MPI_Send(&result, 1, MPI_INT, x, 1, MPI_COMM_WORLD);
 			debug("Got %d from %d", result, status.MPI_SOURCE);
 		}
 	}
+	debug("Master finished");
 }
 
 void slave() {
 	int value;
-	for (value = 0; value < ITERATIONS; value++) {
+	//for (value = 0; value < ITERATIONS; value++) {
+	for(value = 0;;value++) {
+		MPI_Status status;
 		debug("Sending %d", value); 
 		MPI_Send(&value, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+		MPI_Recv(&value, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+		debug("Got data from master: %d", value);
+	}
+
+	if (myrank % 10 == 0) {
+		for (;;) { /* Never exit */
+			MPI_Status status;
+			debug("Sending %d", value); 
+			MPI_Send(&value, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+			value++;
+			debug("Sleeping...");
+		}
 	}
 }
 	
