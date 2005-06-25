4cf8bf7a76df7cbfde71930a1bf8517bf13eca96
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2005-06-25 20:13:05 +0000

    git-svn-id: http://semicomplete.googlecode.com/svn@365 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- xboxproxy/tags/working/autofu.sh
+++
@@ -1,9 +0,0 @@
-set -x
-set -e
-touch NEWS README AUTHORS ChangeLog
-
-aclocal19 || aclocal15 || aclocal
-autoheader259 || autoheader
-autoconf259 || autoconf
-automake19 -a || automake15 -a || automake -a
-
