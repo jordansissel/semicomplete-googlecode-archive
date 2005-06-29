4cee45e65c821a1053946c651f29454412abcdd4
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2005-06-29 01:35:52 +0000

    - Most of the javascript seems to be working...
    git-svn-id: http://semicomplete.googlecode.com/svn@395 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ xmlpresenter/presenter.js
@@ -0,0 +1,92 @@
+var lastslide;
+var oldurl;
+var oldhash;
+var currentslide = 0;
+
+function showslide(id) {
+	if (!id) return;
+	var x = document.getElementById(id);
+
+	if (!lastslide)
+		lastslide = slides[0];
+
+	window.location.hash = "#" + id;
+	//oldurl = location.href;
+	oldhash = window.location.hash;
+	document.getElementById(lastslide).style.display = "none";
+	x.style.display = "block";
+	
+	lastslide = id;
+}
+
+function watchurl() {
+	/* If oldhash is unset, set it to nothing */
+	if (!oldhash) { 
+		var u = location.hash;
+		oldhash = u;
+	}
+
+	if (oldhash != location.hash) {
+		//alert("URL CHANGED: \n" + location.href + "\n vs \n" + oldurl);
+		oldhash = location.hash;
+
+		var i = oldhash.indexOf("#");
+		if (i >= 0) {
+			var id = oldhash.substring(i + 1, oldhash.length);
+			currentslide = id.substring(id.indexOf("_")+1, id.length);
+			currentslide = parseInt(currentslide);
+			if (isNaN(currentslide)) {
+				showslide(slides[0]);
+				currentslide = 0;
+			} else if (currentslide < 0 || currentslide >= slides.length) {
+				showslide(slides[0]);
+				currentslide = 0;
+			} else {
+				showslide(id);
+			}
+		} else {
+			showslide(slides[0]);
+			currentslide = 0;
+		}
+	}
+	setTimeout(watchurl, 100);
+}
+
+function loaded() {
+
+	watchurl();
+	if (!lastslide)
+		lastslide = "slide_0";
+}
+
+function button_next() {
+	if (currentslide < slides.length - 1) {
+		currentslide++;
+		showslide(slides[currentslide]);
+		fix_buttons();
+	}
+}
+
+function button_prev() {
+	if (currentslide > 0) {
+		currentslide--;
+		showslide(slides[currentslide]);
+		fix_buttons();
+	}
+}
+
+function fix_buttons() {
+	if (currentslide >= slides.length - 1) {
+		document.getElementById("id_button_next").src = "images/rightarrow-disabled.png";
+	} else {
+		document.getElementById("id_button_next").src = "images/rightarrow.png";
+	}
+
+	if (currentslide <= 0) {
+		document.getElementById("id_button_prev").src = "images/leftarrow-disabled.png";
+	} else {
+		document.getElementById("id_button_prev").src = "images/leftarrow.png";
+	}
+}
+
+window.onload = loaded;
