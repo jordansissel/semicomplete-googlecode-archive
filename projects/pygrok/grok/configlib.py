a959449cc0f551119483351967c8ec12b9bf08ae
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2006-07-18 00:36:02 +0000

    git-svn-id: http://semicomplete.googlecode.com/svn@250 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- projects/pygrok/grok/configlib.py
+++ projects/pygrok/grok/configlib.py
@@ -1,12 +1,30 @@
+import debuglib
 
 class ConfigParser(object):
-  def __init__(self, filename):
-    self.filename = filename
+  def __init__(self, data=None, file=None):
+    self.data = data
+    self.file = file
+
+  def readfile(self):
+    try:
+      self.fd = open(self.file, "r")
+      self.data = self.read
+      self.fd.close()
+    except Exception, e:
+      debuglib.fatal(e)
+      raise SystemExit(e)
 
   def parse(self):
-    self.fd = open(self.filename)
-    self.fd.close()
+    if self.file:
+      self.readfile()
+
+testconf="""
+  reactions {
+    foo: "bar",
+    baz: "fizz",
+  }
+"""
 
 if __name__=="__main__":
-  c = ConfigParser("/dev/stdin")
+  c = ConfigParser(data=testconf)
   c.parse()
