f868ca9a57443f9a81e30887f83a6dec4af789c4
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2007-02-22 09:20:55 +0000

    - Prepare for 1.2 release
    git-svn-id: http://semicomplete.googlecode.com/svn@1281 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/grok/release.sh
+++ trunk/grok/release.sh
@@ -11,6 +11,8 @@ TMP="/tmp"
 GROK="grok-$1"
 DIR="${TMP}/${GROK}"
 mkdir "$DIR"
-cp -v grok grok.1 grok.conf "$DIR"
+cp -v grok grok.1 grok.conf CHANGELIST "$DIR"
+rsync -a --exclude 'CVS' t "$DIR"
+
 tar -C /tmp -vcf grok-$1.tar.gz "$GROK"
 rm -r $DIR
