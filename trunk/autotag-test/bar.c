402b582e7d9ddb8d5df96c90fee201b7a9a9cbb9
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2007-03-17 01:17:39 +0000

    mv /trunk/autotag-test/ -> /autotag-test/
    git-svn-id: http://semicomplete.googlecode.com/svn@1304 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- trunk/autotag-test/bar.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int razzle() {
-	printf("Hello from razzle()\n");
-	return 0;
-}
--- trunk/autotag-test/foo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "bar.h"
-
-int main() {
-	/* Foo */
-	printf("main says hi...\n");
-	razzle();
-	return 0;
-}
