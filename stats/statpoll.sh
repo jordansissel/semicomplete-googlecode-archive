17ee554b548231aec6200185f59da2cf198c713b
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2007-03-19 06:49:49 +0000

    readable-ized update() and added a colection agent host
    git-svn-id: http://semicomplete.googlecode.com/svn@1338 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- stats/statpoll.sh
+++ stats/statpoll.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+collector="localhost"
+
 update() {
   while true; do
     time=`perl -le 'print time()'`
-    (vmstat -s | tr -d '&;' | gawk '$1 ~ /^[0-9][0-9]*$/ {a=$1; $1=""; gsub(" ","_",$0); print "C_"$0"="a}' | sed -e 's/__/_/;'|tr -d '()[]{}' | tr '\n' '&' ; echo) | sed -e 's,^,http://home.semicomplete.com/~jls/stats/updater.py?,' | xargs -n1 wget -q -O /dev/null
+    (vmstat -s \
+     | tr -d '&;' \
+     | gawk '$1 ~ /^[0-9][0-9]*$/ {a=$1; $1=""; gsub(" ","_",$0); print "C_"$0"="a}' \
+     | sed -e 's/__*/_/;'\
+     | tr -d '()[]{}' \
+     | tr '\n' '&' ; echo) \
+     | sed -e 's,^,'$collector'?,' \
+     | xargs -n1 wget -q -O /dev/null
 
     duration=`perl -le "print 50 - (time() - $time)"`
     sleep $duration
