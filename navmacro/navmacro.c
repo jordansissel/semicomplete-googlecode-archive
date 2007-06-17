9be3f6d24b7fac0e38e17518de9fb5aabc7bc68f
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2007-06-17 09:30:28 +0000

    Figured out how to get X window id from a GTK window (or any widget for that matter)
    git-svn-id: http://semicomplete.googlecode.com/svn@1389 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- navmacro/navmacro.c
+++ navmacro/navmacro.c
@@ -15,8 +15,14 @@ static void destroy( GtkWidget *widget, gpointer   data ) {
   gtk_main_quit ();
 }
 
+static void hello( GtkWidget *widget, gpointer   data ) {
+  //g_print("foo: %d\n", GDK_IS_WINDOW(GTK_WINDOW(GTK_WIDGET(widget)->parent)->frame));
+  g_print("foo: %d\n", GDK_WINDOW_XID(widget->window));
+}
+
 int main( int argc, char **argv ) {
   GtkWidget *window;
+  GtkWidget *button;
   
   gtk_init (&argc, &argv);
 
@@ -27,9 +33,15 @@ int main( int argc, char **argv ) {
   g_signal_connect (G_OBJECT (window), "destroy",
         G_CALLBACK (destroy), NULL);
   
-  gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_UTILITY);
+  gtk_container_set_border_width (GTK_CONTAINER (window), 5);
+  //gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_UTILITY);
+
+  button = gtk_button_new_with_label("foo bar");
+  g_signal_connect(G_OBJECT (button), "clicked", G_CALLBACK (hello), NULL);
+  gtk_container_add (GTK_CONTAINER (window), button);
+  gtk_widget_show(button);
   gtk_widget_show(window);
 
-  gtk_main ();
+  gtk_main();
   return 0;
 }
