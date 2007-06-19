4ad9818fd1a1559711e64dead7fefc198a044108
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2007-06-19 05:46:04 +0000

    Reasonable enough interface for now
    git-svn-id: http://semicomplete.googlecode.com/svn@1400 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- navmacro/navmacro.c
+++ navmacro/navmacro.c
@@ -17,103 +17,47 @@
 
 #define BORDER_SIZE (3)
 
+typedef struct ui {
+  GtkWidget *window;
+  GtkWidget *status_label;
+  GtkWidget *vbox;
+
+  /* Hidden pieces of the UI */
+  GtkWidget *user_input;
+  GtkWidget *fixed_container;
+} ui_t;
+
+ui_t ui;
+
 static struct xdpyinfo {
   Display *display;
   int width;
 } xwin;
 
-static GdkPixmap *pixmap_top;
-static GdkPixmap *pixmap_bottom;
-
 static gboolean
 input_changed_event(GtkWidget *widget, gpointer userdata) {
-  g_print("input\n");
+  const gchar *field_value;
+
+  field_value = gtk_entry_get_text(GTK_ENTRY(widget));
+  gtk_label_set_markup(GTK_LABEL(ui.status_label), field_value);
   return TRUE;
 }
 
 static gboolean
 input_key_press(GtkWidget *widget, GdkEventKey *event, gpointer data) {
-  g_print("key %d\n", event->hardware_keycode);
-  // keycode 9 == Escape
+
+  if (event->hardware_keycode == 9) // Escape
+    gtk_main_quit ();
+
+  //g_print("key %d\n", event->hardware_keycode);
 }
 
-static gboolean
-configure_event_border_top( GtkWidget *widget, GdkEventConfigure *event ) {
-  if (pixmap_top)
-    g_object_unref(pixmap_top);
-
-  pixmap_top = gdk_pixmap_new(widget->window, widget->allocation.width,
-                          BORDER_SIZE, -1);
-  gdk_draw_rectangle(pixmap_top,
-          widget->style->black_gc,
-          TRUE,
-          0, 0,
-          widget->allocation.width,
-          BORDER_SIZE);
-
-  gdk_draw_line(pixmap_top,
-          widget->style->white_gc,
-          0, 0,
-          widget->allocation.width,
-          0);
-
-  return TRUE;
-}
-
-static gboolean
-configure_event_border_bottom( GtkWidget *widget, GdkEventConfigure *event ) {
-  if (pixmap_bottom)
-    g_object_unref(pixmap_bottom);
-
-  pixmap_bottom = gdk_pixmap_new(widget->window, widget->allocation.width,
-                          BORDER_SIZE, -1);
-  gdk_draw_rectangle(pixmap_bottom,
-          widget->style->black_gc,
-          TRUE,
-          0, 0,
-          widget->allocation.width,
-          BORDER_SIZE);
-
-  gdk_draw_line(pixmap_bottom,
-          widget->style->white_gc,
-          0, BORDER_SIZE - 1,
-          widget->allocation.width, BORDER_SIZE - 1);
-
-
-  return TRUE;
-}
-
-static gboolean
-expose_event_border_top( GtkWidget *widget, GdkEventExpose *event )
-{
-  gdk_draw_drawable(widget->window,
-        widget->style->fg_gc[GTK_WIDGET_STATE (widget)],
-        pixmap_top,
-        event->area.x, event->area.y,
-        event->area.x, event->area.y,
-        event->area.width, event->area.height);
-
-  return FALSE;
-}
-
-static gboolean
-expose_event_border_bottom( GtkWidget *widget, GdkEventExpose *event )
-{
-  gdk_draw_drawable(widget->window,
-        widget->style->fg_gc[GTK_WIDGET_STATE (widget)],
-        pixmap_bottom,
-        event->area.x, event->area.y,
-        event->area.x, event->area.y,
-        event->area.width, event->area.height);
-
-  return FALSE;
-}
 
 static gboolean delete_event( GtkWidget *widget, GdkEvent *event, gpointer data ) {
     return FALSE; /* Call destroy on window closure (deletion) */
 }
 
-static void activate( GtkWidget *widget, gpointer   data ) {
+static void realize_mainwindow( GtkWidget *widget, gpointer data) {
   int window_id;
   XSetWindowAttributes winattr;
   GdkScreen *gdk_screen;
@@ -147,59 +91,51 @@ static void destroy( GtkWidget *widget, gpointer   data ) {
 }
 
 int main( int argc, char **argv ) {
-  GtkWidget *window;
-  GtkWidget *input;
-  GtkWidget *label;
-  GtkWidget *draw_top;
-  GtkWidget *draw_bottom;
-  GtkWidget *vbox;
-  
   gtk_init (&argc, &argv);
 
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER_ALWAYS);
-  g_signal_connect(G_OBJECT (window), "delete_event",
+  ui.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_position(GTK_WINDOW(ui.window), GTK_WIN_POS_CENTER_ALWAYS);
+  g_signal_connect(G_OBJECT (ui.window), "delete_event",
                    G_CALLBACK (delete_event), NULL);
-  g_signal_connect(G_OBJECT (window), "destroy",
+  g_signal_connect(G_OBJECT (ui.window), "destroy",
                    G_CALLBACK (destroy), NULL);
-  g_signal_connect(G_OBJECT(window), "realize",
-                   G_CALLBACK(activate), NULL);
-  g_signal_connect(G_OBJECT(window), "configure_event",
+  g_signal_connect(G_OBJECT(ui.window), "realize",
+                   G_CALLBACK(realize_mainwindow), NULL);
+  g_signal_connect(G_OBJECT(ui.window), "configure_event",
                    G_CALLBACK(configure_event_mainwindow), NULL);
 
-  vbox = gtk_vbox_new(FALSE, 0);
+  ui.vbox = gtk_vbox_new(FALSE, 0);
   
-  input = gtk_entry_new();
-  g_signal_connect(G_OBJECT(input), "changed", G_CALLBACK (input_changed_event), NULL);
-  g_signal_connect_after(G_OBJECT(input), "key-press-event", G_CALLBACK (input_key_press), NULL);
-  gtk_widget_set_size_request(input, -1, 1);
+  ui.user_input = gtk_entry_new();
+  g_signal_connect(G_OBJECT(ui.user_input), "changed", G_CALLBACK (input_changed_event), NULL);
+  g_signal_connect_after(G_OBJECT(ui.user_input), "key-press-event", G_CALLBACK (input_key_press), NULL);
   
-  label = gtk_label_new("");
-  gtk_label_set_markup(GTK_LABEL(label), "foo bar <b>baz</b>");
+  ui.status_label = gtk_label_new("");
+  static GdkColor label_bgcolor;
+  gdk_color_parse("#DFDF60", &label_bgcolor);
+  //GdkColormap *cmap = gdk_colormap_get_system();
+  //gdk_colormap_alloc_color(cmap, &label_bgcolor, TRUE, TRUE);
+  gtk_widget_modify_base(ui.user_input, GTK_STATE_NORMAL, &label_bgcolor);
 
-  draw_top = gtk_drawing_area_new();
-  draw_bottom = gtk_drawing_area_new();
-  gtk_widget_set_size_request(draw_top, -1, BORDER_SIZE);
-  gtk_widget_set_size_request(draw_bottom, -1, BORDER_SIZE);
+  PangoFontDescription *pfd;
+  pfd = pango_font_description_new();
+  pango_font_description_set_size(pfd, 14 * PANGO_SCALE);
+  gtk_widget_modify_font(ui.user_input, pfd);
 
-  g_signal_connect(G_OBJECT(draw_top), "configure_event",  
-                   G_CALLBACK(configure_event_border_top), NULL);
-  g_signal_connect(G_OBJECT(draw_bottom), "configure_event",  
-                   G_CALLBACK(configure_event_border_bottom), NULL);
-  g_signal_connect(G_OBJECT(draw_top), "expose_event",  
-                   G_CALLBACK(expose_event_border_top), NULL);
-  g_signal_connect(G_OBJECT(draw_bottom), "expose_event",  
-                   G_CALLBACK(expose_event_border_bottom), NULL);
+  gtk_container_set_border_width(GTK_CONTAINER(ui.window), 5);
 
-  gtk_container_add(GTK_CONTAINER(vbox), draw_top);
-  gtk_container_add(GTK_CONTAINER(vbox), input);
-  gtk_container_add(GTK_CONTAINER(vbox), label);
-  gtk_container_add(GTK_CONTAINER(vbox), draw_bottom);
+  /* Hide the input field */
+  //gtk_widget_set_size_request(ui.user_input, -1, 1);
+  //ui.fixed_container = gtk_fixed_new();
+  //gtk_fixed_put(GTK_FIXED(ui.fixed_container), ui.user_input, -50, -50);
+  //gtk_container_add(GTK_CONTAINER(ui.vbox), ui.fixed_container);
+  //gtk_container_add(GTK_CONTAINER(ui.vbox), ui.status_label);
 
-  gtk_container_add(GTK_CONTAINER(window), vbox);
+  gtk_container_add(GTK_CONTAINER(ui.vbox), ui.user_input);
+  gtk_container_add(GTK_CONTAINER(ui.window), ui.vbox);
 
-  gtk_widget_show_all(window);
-  gtk_widget_grab_focus(GTK_WIDGET(input));
+  gtk_widget_show_all(ui.window);
+  gtk_widget_grab_focus(GTK_WIDGET(ui.user_input));
 
   gtk_main();
   return 0;
