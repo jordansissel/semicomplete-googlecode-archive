ab074501fabc0aa466af982e7a8c5d9851917324
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2007-08-03 21:22:01 +0000

    - open(2) override.
    git-svn-id: http://semicomplete.googlecode.com/svn@1504 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- liboverride/socket.c
+++ liboverride/socket.c
@@ -2,6 +2,11 @@
 #include <dlfcn.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <fcntl.h>
+
+#define OVERRIDE_BIND
+#ifdef OVERRIDE_BIND
+
 
 int bind(int s, const struct sockaddr *addr, socklen_t addrlen) {
   void *handle = NULL;
@@ -15,5 +20,25 @@ int bind(int s, const struct sockaddr *addr, socklen_t addrlen) {
   real_func = dlsym(handle, "bind");
 
   printf("bind called on socket %d\n", s);
+  dlclose(handle);
   return real_func(s, addr, addrlen);
+}
+
+#endif /* OVERRIDE_BIND */
+
+int open(const char *path, int flags, ...) {
+  void *handle = NULL;
+  int (*real_func)(const char *path, int flags, ...);
+  handle = dlopen("/lib/libc.so.6", RTLD_LAZY);
+
+  if (handle == NULL) {
+    fprintf(stderr, "Failed to dlopen libc\n");
+    return -1;
+  }
+  real_func = dlsym(handle, "open");
+
+  printf("Open: %s\n", path);
+  dlclose(handle);
+  return real_func(path, flags);
+  
 }
