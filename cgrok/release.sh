649c8047784f89c06d7333ef7c19f19bf186339b
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-01-17 08:52:56 +0000

    - Put together the release toolset
    git-svn-id: http://semicomplete.googlecode.com/svn@1754 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ cgrok/release.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ $# -ne 1 ] ; then 
+  set -- $(date "+%Y%m%d")
+  echo "No version specified, using $1"
+fi
+
+if [ -z "$PACKAGE" ] ; then
+  echo "Must specify package name in environment."
+  echo "PACKAGE= ???"
+  exit 1
+fi
+
+if [ ! -f "FILES" ] ; then
+  echo "Missing FILES file which should contain the list of things to put in this package"
+  exit 1
+fi
+
+TMP="/tmp"
+PACKAGE="$PACKAGE-$1"
+DIR="${TMP}/${PACKAGE}"
+mkdir "$DIR"
+rsync -a --cvs-exclude --include-from FILES \
+  --exclude .svn --exclude /sandbox/ . "$DIR"
+
+# gnu tar sucks, tar -C /tmp doesn't actually change directories for tar
+# creation?
+tar -C /tmp/ -zvcf "${PACKAGE}.tar.gz" "$PACKAGE"
+rm -r "$DIR"
+
