cc83bc58b085937cd959f47bbd373533efa2a687
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-05-16 00:17:43 +0000

    git-svn-id: http://semicomplete.googlecode.com/svn@1849 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- bungiemap/annotate.pl
+++ bungiemap/annotate.pl
@@ -8,6 +8,8 @@ use POSIX qw(strftime);
 my $font = "/usr/lib/j2re1.5-sun/lib/fonts/LucidaSansRegular.ttf";
 
 my $tmp = $ARGV[0];
+shift(@ARGV);
+my @idx = @ARGV;
 
 sub blend {
   my ($idx, $image, @images) = @_;
@@ -30,21 +32,21 @@ sub blend {
 }
 
 my @images = glob("mapdata/nightmap.*.jpg");
-my $idx = -1;
-foreach my $i (@images) {
-  $idx++;
+
+for my $idx (@idx) {
+  my $i = $images[$idx];
   my @f = split("/", $i);
   my $f = $f[1];
   my $output = "$tmp/input_$f";
   my @ostat = stat($output); my $omtime = $ostat[9] || 0;
   my @istat = stat($i); my $imtime = $istat[9] || 0;
-  next if ($omtime > $imtime);
+  exit if ($omtime > $imtime);
   my $image = Image::Magick->new();
 
   my @time = split(/\./, $f);
-  print STDERR "$f ", join("/", @time), "\n";
   $image->Read("$i");
-  next if ($image->Get("width") != 602);
+  exit if ($image->Get("width") != 602);
+  print STDERR "$f ", join("/", @time), " / $idx\n";
 
   my $text = strftime("%Y/%m/%d %H:%M GMT %z", localtime($time[1]));
   my $blend_image = blend($idx, $image, @images);
@@ -56,5 +58,3 @@ foreach my $i (@images) {
 
   $image->Write($output);
 }
-
-#system("mencoder mf://input_*.jpg -mf w=602:h=250:fps=25:type=jpg -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o output.avi");
