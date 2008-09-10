9fa5dc39d8a4064cc11a2b3b5ea29f3d2dbfa47f
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-09-10 05:19:20 +0000

    - Start working on tests
    git-svn-id: http://semicomplete.googlecode.com/svn@1961 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ cgrok/grokpcre/test/test_stringhelper.c
@@ -0,0 +1,21 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "../stringhelper.h"
+
+void test_substr_replace(void) {
+  char *source = calloc(1, 1024);
+  char *dest = calloc(1, 1024);
+  int slen = -1, dlen = -1, salloc = 1024;
+
+  sprintf(source, "world");
+  sprintf(dest, "hello there");
+
+  substr_replace(&dest, &slen, &salloc, 6, strlen(dest), source, dlen);
+  printf("%s\n", dest);
+
+  free(source);
+  free(dest);
+}
