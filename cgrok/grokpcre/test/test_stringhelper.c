013c48d11da9b06f3323fea479165278fc4d85ba
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-09-10 06:08:46 +0000

    - Add a substr_replace test where the pointer passed is set to null
    git-svn-id: http://semicomplete.googlecode.com/svn@1964 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- cgrok/grokpcre/test/test_stringhelper.c
+++ cgrok/grokpcre/test/test_stringhelper.c
@@ -5,18 +5,33 @@
 
 #include "../stringhelper.h"
 
-void test_substr_replace(void) {
+void test_unmoving_insertion(void) {
   char *source = calloc(1, 1024);
   char *dest = calloc(1, 1024);
-  int slen = -1, dlen = -1, salloc = 1024;
+  int slen = -1, dlen = -1, dalloc = 1024;
 
   sprintf(source, "world");
   sprintf(dest, "hello there");
 
-  substr_replace(&dest, &slen, &salloc, 6, strlen(dest), source, dlen);
-  printf("%s\n", dest);
+  substr_replace(&dest, &dlen, &dalloc, 6, strlen(dest), source, slen);
+  CU_ASSERT(!strcmp(dest, "hello world"));
+  CU_ASSERT(dalloc == 1024);
+  CU_ASSERT(dlen == 11);
 
   free(source);
   free(dest);
 }
 
+void test_alloc_and_insert_from_null_dest(void) {
+  char *source = "hello world";
+  char *dest = NULL;
+  int dlen = 0, dalloc = 0;
+
+  substr_replace(&dest, &dlen, &dalloc, 0, 0, source, -1);
+
+  CU_ASSERT(!strcmp(dest, source));
+  CU_ASSERT(dlen == strlen(source));
+  CU_ASSERT(dalloc > dlen);
+
+  free(dest);
+}
