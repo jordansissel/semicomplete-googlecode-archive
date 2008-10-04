0c35e2885e28eff07da5939c44b3534569a68771
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-10-04 09:34:54 +0000

    - add grok_predicate_{num,str}compare_init
    git-svn-id: http://semicomplete.googlecode.com/svn@1994 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- cgrok/predicates.h
+++ cgrok/predicates.h
@@ -8,17 +8,12 @@
  * Activate with operator '=~'
  */
 
-//typedef struct grok_predicate_regexp {
-  //pcre *re;
-  //char *pattern;
-  //int negative_match;
-//} grok_predicate_regexp_t;
-
-//int grok_predicate_regexp(grok_t *grok, grok_capture_t *gct,
-                          //const char *subject, int start, int end);
-
 int grok_predicate_regexp_init(grok_t *grok, grok_capture_t *gct,
                                const char *args);
+int grok_predicate_numcompare_init(grok_t *grok, grok_capture_t *gct,
+                                   const char *args);
+int grok_predicate_strcompare_init(grok_t *grok, grok_capture_t *gct,
+                                   const char *args);
 
 
 #endif /* _PREDICATES_H_ */
