95b8c539b547c11e9ebbef167bdfd2c2f00177b3
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-10-04 07:42:47 +0000

    - Add numerical comparisons < <= > >= == !=   Optimize by using long instead of double when the comparator is not a   floating point (aka 10 vs 10.)
    git-svn-id: http://semicomplete.googlecode.com/svn@1987 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- cgrok/predicates.c
+++ cgrok/predicates.c
@@ -9,6 +9,25 @@ static pcre *regexp_predicate_op = NULL;
 
 static void grok_predicate_regexp_global_init(void);
 
+typedef struct grok_predicate_regexp {
+  pcre *re;
+  char *pattern;
+  int negative_match;
+} grok_predicate_regexp_t;
+
+typedef struct grok_predicate_numcompare {
+  enum { OP_LT, OP_GT, OP_GE, OP_LE, OP_EQ, OP_NE } op;
+  enum { DOUBLE, LONG } type;
+  union {
+    long lvalue;
+    double dvalue;
+  } u;
+} grok_predicate_numcompare_t;
+
+int grok_predicate_regexp(grok_t *grok, grok_capture_t *gct,
+                          const char *subject, int start, int end);
+int grok_predicate_numcompare(grok_t *grok, grok_capture_t *gct,
+                              const char *subject, int start, int end);
 
 int grok_predicate_regexp(grok_t *grok, grok_capture_t *gct,
                           const char *subject, int start, int end) {
@@ -59,7 +78,8 @@ int grok_predicate_regexp_init(grok_t *grok, grok_capture_t *gct,
     return;
   }
 
-  fprintf(stderr, "Compiled regex for '%s': '%s'\n", gct->name, gprt->pattern);
+  //fprintf(stderr, "Compiled regex for '%s': '%s'\n", gct->name, gprt->pattern);
+  gct->predicate_func = grok_predicate_regexp;
   gct->extra = gprt;
 }
 
@@ -75,3 +95,92 @@ static void grok_predicate_regexp_global_init(void) {
     }
   }
 }
+
+int grok_predicate_numcompare_init(grok_t *grok, grok_capture_t *gct,
+                                   const char *args) {
+  grok_predicate_numcompare_t *gpnt;
+  gpnt = calloc(1, sizeof(grok_predicate_numcompare_t));
+  //fprintf(stderr, "numcompare init: '%s'\n", args);
+
+  int pos = 1;
+
+  switch (args[0]) {
+    case '<':
+      if (args[1] == '=') { gpnt->op = OP_LE; pos = 2; }
+      else gpnt->op = OP_LT;
+      break;
+    case '>':
+      if (args[1] == '=') { gpnt->op = OP_GE; pos = 2; }
+      else gpnt->op = OP_GT;
+      break;
+    case '=':
+      if (args[1] == '=') { gpnt->op = OP_EQ; pos = 2; }
+      else {
+        fprintf(stderr, "Invalid predicate: '%s'\n", args);
+        return -1;
+      }
+      break;
+    case '!':
+      if (args[1] == '=') { gpnt->op = OP_NE; pos = 2; }
+      else {
+        fprintf(stderr, "Invalid predicate: '%s'\n", args);
+        return -1;
+      }
+      break;
+    default:
+      fprintf(stderr, "Invalid predicate: '%s'\n", args);
+  }
+
+  /* Optimize and use long type if the number is not a float (no period) */
+  if (strchr(args, '.') == NULL) {
+    gpnt->type = LONG;
+    gpnt->u.lvalue = strtol(args + pos, NULL, 0);
+  } else {
+    gpnt->type = DOUBLE;
+    gpnt->u.dvalue = strtod(args + pos, NULL);
+    //fprintf(stderr, "numcompare_init: %s %f\n", args + pos, gpnt->u.dvalue);
+  }
+
+  gct->predicate_func = grok_predicate_numcompare;
+  gct->extra = gpnt;
+}
+
+int grok_predicate_numcompare(grok_t *grok, grok_capture_t *gct,
+                              const char *subject, int start, int end) {
+  grok_predicate_numcompare_t *gpnt = (grok_predicate_numcompare_t *)gct->extra;
+  int ret;
+
+  //fprintf(stderr, "numcompare: %*.s\n", (end - start), subject + start);
+
+  if (gpnt->type == DOUBLE) {
+    double a = strtod(subject + start, NULL);
+    double b = gpnt->u.dvalue;
+    //printf("%f vs %f\n", a, b);
+    switch (gpnt->op) {
+      case OP_LT: ret = (a < b); break;
+      case OP_GT: ret = (a > b); break;
+      case OP_GE: ret = (a >= b); break;
+      case OP_LE: ret = (a <= b); break;
+      case OP_EQ: ret = (a == b); break;
+      case OP_NE: ret = (a != b); break;
+    }
+  } else {
+    long a = strtol(subject + start, NULL, 0);
+    long b = gpnt->u.lvalue;
+    //printf("%ld vs %ld\n", a, b);
+    switch (gpnt->op) {
+      case OP_LT: ret = (a < b); break;
+      case OP_GT: ret = (a > b); break;
+      case OP_GE: ret = (a >= b); break;
+      case OP_LE: ret = (a <= b); break;
+      case OP_EQ: ret = (a == b); break;
+      case OP_NE: ret = (a != b); break;
+    }
+  }
+
+  //fprintf(stderr, "numcompare: %d / %s", ret, subject);
+  /* grok predicates should return 0 for success and comparisons return 1 for
+   * success */
+  return ret == 0;
+}
+
