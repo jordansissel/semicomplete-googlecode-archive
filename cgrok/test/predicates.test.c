d8291ffef01f42c5f92fb26f053d6c147cdb5d16
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-10-04 09:39:59 +0000

    - Add more compile predicate tests - Add numcompare_gt_double test - Don't prefix test output with 'make test'
    git-svn-id: http://semicomplete.googlecode.com/svn@1997 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- cgrok/test/predicates.test.c
+++ cgrok/test/predicates.test.c
@@ -1,28 +1,27 @@
 #include "grok.h"
-
-#define INIT \
-    grok_t grok; \
-    grok_init(&grok);
-
-#define ASSERT_MATCHFAIL(str) CU_ASSERT(grok_exec(&grok, str, NULL) < 0)
-#define ASSERT_MATCHOK(str) CU_ASSERT(grok_exec(&grok, str, NULL) >= 0)
-
+#include "test.h"
 
 void test_grok_with_predicate_compile_succeeds(void) {
   INIT;
   grok_patterns_import_from_string(&grok, "WORD \\b\\w+\\b");
 
-  CU_ASSERT(grok_compile(&grok, "%{WORD=~/test/}") == 0);
-  CU_ASSERT(grok_compile(&grok, "%{WORD>30}") == 0);
+  ASSERT_COMPILEOK("%{WORD=~/test/}");
+  ASSERT_COMPILEOK("%{WORD!~/test/}");
+  ASSERT_COMPILEOK("%{WORD>30}");
+  ASSERT_COMPILEOK("%{WORD>=30}");
+  ASSERT_COMPILEOK("%{WORD<30}");
+  ASSERT_COMPILEOK("%{WORD<=30}");
+  ASSERT_COMPILEOK("%{WORD==30}");
+  ASSERT_COMPILEOK("%{WORD!=30}");
 
-  grok_free(&grok);
+  CLEANUP;
 }
 
 void test_grok_with_numcompare_gt_normal(void) {
   INIT;
-  grok_patterns_import_from_file(&grok, "../pcregrok_patterns");
+  IMPORT_PATTERNS_FILE;
 
-  grok_compile(&grok, "^%{NUMBER>10}$");
+  (void) grok_compile(&grok, "^%{NUMBER>10}$");
 
   ASSERT_MATCHFAIL("0");
   ASSERT_MATCHFAIL("1");
@@ -32,17 +31,47 @@ void test_grok_with_numcompare_gt_normal(void) {
   ASSERT_MATCHFAIL("0.2");
   ASSERT_MATCHFAIL("9.95");
 
-  ASSERT_MATCHOK("10.1")
-  ASSERT_MATCHOK("10.2")
+  /* Should fail since '10' means we use a long, not a double */
+  ASSERT_MATCHFAIL("10.1")
+  ASSERT_MATCHFAIL("10.2")
+
   ASSERT_MATCHOK("11.2")
   ASSERT_MATCHOK("4425.334")
   ASSERT_MATCHOK("11");
   ASSERT_MATCHOK("15");
+
+  CLEANUP;
+}
+
+void test_grok_with_numcompare_gt_double(void) {
+  INIT;
+  IMPORT_PATTERNS_FILE;
+
+  (void) grok_compile(&grok, "^%{NUMBER>10.0}$");
+
+  ASSERT_MATCHFAIL("0");
+  ASSERT_MATCHFAIL("1");
+  ASSERT_MATCHFAIL("9");
+  ASSERT_MATCHFAIL("10");
+  ASSERT_MATCHFAIL("5.5");
+  ASSERT_MATCHFAIL("0.2");
+  ASSERT_MATCHFAIL("9.95");
+
+  /* Should pass since '10.0' means we use a double */
+  ASSERT_MATCHOK("10.1")
+  ASSERT_MATCHOK("10.2")
+
+  ASSERT_MATCHOK("11.2")
+  ASSERT_MATCHOK("4425.334")
+  ASSERT_MATCHOK("11");
+  ASSERT_MATCHOK("15");
+
+  CLEANUP;
 }
 
 void test_grok_with_numcompare_gt_hex(void) {
   INIT;
-  grok_patterns_import_from_file(&grok, "../pcregrok_patterns");
+  IMPORT_PATTERNS_FILE;
 
   grok_compile(&grok, "%{BASE16FLOAT>0x000A}");
 
@@ -58,4 +87,13 @@ void test_grok_with_numcompare_gt_hex(void) {
   ASSERT_MATCHOK("0x0B");
   ASSERT_MATCHOK("0xB");
   ASSERT_MATCHOK("0xFF");
+
+  CLEANUP;
 }
+
+void test_grok_numcompare_general(void) {
+  INIT;
+
+  CLEANUP;
+}
+
