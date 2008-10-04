00cb46c5385666c85776e4c88ea8bfd0a744c4ab
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-10-04 12:23:03 +0000

    - const char *buffer
    git-svn-id: http://semicomplete.googlecode.com/svn@2003 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- cgrok/grokre.h
+++ cgrok/grokre.h
@@ -4,7 +4,7 @@
 void grok_init(grok_t *grok);
 void grok_free(grok_t *grok);
 void grok_patterns_import_from_file(grok_t *grok, const char *filename);
-void grok_patterns_import_from_string(grok_t *grok, char *buffer);
+void grok_patterns_import_from_string(grok_t *grok, const char *buffer);
 
 int grok_compile(grok_t *grok, const char *pattern);
 int grok_exec(grok_t *grok, const char *text, grok_match_t *gm);
