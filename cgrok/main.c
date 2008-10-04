725f58bb97e0b4fa8386dda5d470422cddaf1570
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-10-04 12:22:50 +0000

    git-svn-id: http://semicomplete.googlecode.com/svn@2002 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- cgrok/main.c
+++ cgrok/main.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include "grok.h"
 
+#include <sys/select.h>
+
+
 int main(int argc, char **argv) {
   grok_t grok;
   grok_match_t gm;
@@ -16,18 +19,21 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  printf("Compile: %d\n", grok_compile(&grok, argv[1]));
-
+  grok_compile(&grok, argv[1]);
  
-  while (fgets(buf, 1024, stdin)) {
-    int start, end, ret;
-    ret = grok_exec(&grok, buf, &gm);
-    start = grok.pcre_capture_vector[0];
-    end = grok.pcre_capture_vector[1];
-    printf("Match: %d\n", ret);
-    if (ret >= 0)
-      printf("Substr: '%.*s'\n", end - start, buf + start);
+  while (fgets(buf, 4095, stdin)) {
+    //int start, end, ret;
+    int ret;
+    buf[4096] = '\0';
+    //ret = grok_exec(&grok, buf, &gm);
+    ret = grok_exec(&grok, buf, NULL);
+    //start = grok.pcre_capture_vector[0];
+    //end = grok.pcre_capture_vector[1];
+    if (ret >= 0) {
+      printf("%s", buf);
+    }
   }
 
+  grok_free(&grok);
   return 0;
 }
