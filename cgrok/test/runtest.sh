9eb9131a1187e711a3c5a72a3fd501d42af9c029
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-10-09 11:16:56 +0000

    - test runner
    git-svn-id: http://semicomplete.googlecode.com/svn@2030 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ cgrok/test/runtest.sh
@@ -0,0 +1,16 @@
+TEST=$1
+
+if [ -z "$TEST" ] ; then
+  echo "Usage: $0 <test>"
+  exit 1
+fi
+
+cout=`make clean $TEST 2>&1`
+if [ $? -ne 0 ]; then
+  echo "$cout"
+  echo "Compile for $TEST failed."
+else
+  ./$TEST | egrep '^  (Test:|  [0-9])'
+fi
+
+
