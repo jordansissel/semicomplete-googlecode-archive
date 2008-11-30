770536cf4ff75008b4584b07740aa3757a9cbb90
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2008-11-30 11:17:25 +0000

    - Add test for C escaping
    git-svn-id: http://semicomplete.googlecode.com/svn@2114 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- cgrok/test/stringhelper.test.c
+++ cgrok/test/stringhelper.test.c
@@ -4,7 +4,7 @@
 
 #include "stringhelper.h"
 
-void test_unmoving_insertion(void) {
+void test_substr_replace_unmoving_insertion(void) {
   char *source = calloc(1, 1024);
   char *dest = calloc(1, 1024);
   int slen = -1, dlen = -1, dalloc = 1024;
@@ -21,7 +21,7 @@ void test_unmoving_insertion(void) {
   free(dest);
 }
 
-void test_alloc_and_insert_from_null_dest(void) {
+void test_substr_replace_alloc_and_insert_from_null_dest(void) {
   char *source = "hello world";
   char *dest = NULL;
   int dlen = 0, dalloc = 0;
@@ -35,7 +35,7 @@ void test_alloc_and_insert_from_null_dest(void) {
   free(dest);
 }
 
-void test_remove(void) {
+void test_substr_replace_remove(void) {
   char *source = strdup("hello world");
   int len = strlen(source);
   int size = len + 1;
@@ -46,7 +46,7 @@ void test_remove(void) {
   CU_ASSERT(!strcmp(source, "hello"));
 }
 
-void test_lenchange(void) {
+void test_substr_replace_lenchange(void) {
   char *source = strdup("hello world test");
   int len = strlen(source);
   int size = len + 1;
@@ -54,4 +54,46 @@ void test_lenchange(void) {
   CU_ASSERT(len == 16);
   substr_replace(&source, &len, &size, 5, len, "", 0);
   CU_ASSERT(len == 5);
+}
+
+void test_string_escape_c(void) {
+  struct {
+    char *input;
+    char *output;
+  } data[] = {
+    { "no change", "no change" },
+    { "quoty \" ?", "quoty \\\" ?" },
+    { "test \n", "test \\n" },
+    { "test \r", "test \\r" },
+    { "test \f", "test \\f" },
+    { "test \a", "test \\a" },
+    { "test \b", "test \\b" },
+    { "test \\", "test \\\\" },
+    { NULL, NULL },
+  };
+
+  int i = 0;
+  for (i = 0; data[i].input != NULL ; i++) {
+    int len, size;
+    char *s = strdup(data[i].input);
+    len = strlen(s);
+    size = len + 1;
+
+    string_escape(&s, &len, &size, "\\\"", 2, ESCAPE_LIKE_C);
+    string_escape(&s, &len, &size, "", 0, ESCAPE_LIKE_C | ESCAPE_NONPRINTABLE);
+
+    //printf("\n");
+    //printf("'%s' vs '%s' ('%s')\n", data[i].input, s, data[i].output);
+    //printf("\n");
+    if (strcmp(s, data[i].output)) {
+      printf("\n");
+      printf("'%s' vs '%s' ('%s')\n", data[i].input, s, data[i].output);
+      printf("\n");
+    }
+    CU_ASSERT(!strcmp(s, data[i].output));
+    
+    free(s);
+  }
+
+
 }
