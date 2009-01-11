784b540fdbaf76a9419917246c33c69898c677db
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2009-01-11 07:45:04 +0000

    git-svn-id: http://semicomplete.googlecode.com/svn@2144 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- codesamples/libevent-filefollow.c
+++ codesamples/libevent-filefollow.c
@@ -11,6 +11,10 @@
 
 #include <event.h>
 
+void bufwrite(struct bufferevent *bev, void *data) {
+  printf("OK to write\n");
+}
+
 void bufread(struct bufferevent *bev, void *data) {
   char *line;
   while ( (line = evbuffer_readline(bev->input)) != NULL ) {
@@ -24,18 +28,22 @@ int main() {
   struct event ev;
   int fd;
 
+  fd = open("/var/log/messages", O_RDONLY);
+
   int c[2];
   pipe(c);
   event_init();
 
   /* have a bufferevent watch our child's stdout */
-  bev = bufferevent_new(c[0], bufread, NULL, NULL, NULL);
+  bev = bufferevent_new(c[0], bufread, bufwrite, NULL, NULL);
   bufferevent_enable(bev, EV_READ);
   //struct event ev;
 
   //fd = open("/var/log/messages", O_RDONLY);
   //timeoutk
-  write(c[1], "Hello there\n", 12);
+  //write(c[1], "Hello there\n", 12);
+  //bufferevent_write(bev, "foo\n", 5);
+  evbuffer_read(EVBUFFER_OUTPUT(bev), fd, 1000);
 
   //lseek(fd, 0, SEEK_SET);
   event_dispatch();
