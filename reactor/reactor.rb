42c2956309eae13a1d18070343c6640c6358d933
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2009-03-23 07:04:45 +0000

    git-svn-id: http://semicomplete.googlecode.com/svn@2167 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- reactor/reactor.rb
+++ reactor/reactor.rb
@@ -1,66 +1,120 @@
 #!/usr/bin/env ruby
+# TODO(sissel): support nesting and array indexing in 'key'
+# Converting the JSON object to xml and permitting xpath would be nice
+# For example: foo/bar[0]/baz will fetch
+# { "foo": { "bar" : [ { "baz": 3 }, foo ] } } 
+# and get you: 3
+# Or something...
 
 require "yaml"
+require "time"
 
-class Reactor
-  attr_accessor :threshold
-  attr_accessor :keys
+class ReactorData
+  attr_accessor :timestamp
+  attr_accessor :input
+end
 
-  def initialize
-    @history = []
-    @value = Hash.new { |h,k| h[k] = 0 }
-    #@conditions = []
+class TimeParserError < RuntimeError; end
+
+class TimeParser
+  attr_accessor :format
+  attr_accessor :key
+
+  def initialize(format, key)
+    @format = format
+    @key = key
   end
 
-  def feed(input)
-    key = key_str(input)
-    @value[key] += 1
-    @history << input
-    react(input) if @value[key] >= @threshold
-  end
+  def parse(input)
+    if !input.has_key?(@key)
+      raise TimeParserError, "Input has no timestamp key '#{@key}'"
+    end
 
-  def react(input)
-    key = key_str(input)
-    puts "Threshold exceeded: #{@value[key]} > #{@threshold}"
-    puts input.obj.inspect
-    @value[key] = 0
-  end
-
-  def key_str(input)
-    @keys.collect { |k| input.obj[k] }.join(" ")
+    return Time.parse(input[@key], @format)
   end
 end
 
-class ReactorInput
-  attr_accessor :obj
-  attr_accessor :timestamp
+class Reactor
+  attr_accessor :action
+  attr_accessor :interval
+  attr_accessor :keys
+  attr_accessor :threshold
 
-  def initialize(obj=nil, timestamp=nil)
-    @obj = obj || Hash.new
-    @timestamp = timestamp || Time.now
+  def initialize(config)
+    @action = config["action"]
+    @interval = config["interval"].to_i
+    # TODO(sissel): Accept yaml sequence, too, for keys?
+    @keys = config["key"].split(",")
+    @threshold = config["threshold"].to_i
+
+    if config.has_key?("timestamp-key")
+      @timeparser = TimeParser.new(config["timestamp-format"],
+                                   config["timestamp-key"])
+    else
+      @timeparser = nil
+    end
+
+    #@history = HistoryTracker.new
+    @history = []
+    @value = Hash.new { |h,k| h[k] = 0 }
   end
 
+  def feed(input)
+    return unless @keys.collect { |k| input.has_key?(k) }.reduce { |a,b| a & b }
+    input = process(input)
+    key = keygen(input)
+    @value[key] += 1
+
+    rh = ReactorData.new
+    rh.input = input
+    rh.timestamp = @timeparser ? @timeparser.parse(input) : Time.now
+    @history << rh
+    # If the first history entry older than the age range starting at 
+    # rh.timestamp (ending in rh.timestmap - @interval), delete it.
+    oldrh = @history[0]
+
+    # Special case. If we have no threshold but have an interval,
+    # If we hit interval, react on this key exactly as if we had hit the
+    # threshold.
+    if oldrh.timestamp < (rh.timestamp - @interval)
+      oldkey = keygen(oldrh.input)
+      @value[oldkey] -= 1
+      @history.shift
+    end
+
+    react(key) if @value[key] >= @threshold
+  end
+
+  def react(key)
+    puts "Threshold exceeded: #{@value[key]} > #{@threshold}"
+    puts key.inspect
+    @value[key] = 0
+  end
+
+  def process(input)
+    # make a new object with only the 'keys' key:value pairs present.
+    input = Hash[*keys.collect { |k| [k, input[k]] }.flatten ]
+    return input
+  end
+
+  def keygen(input)
+    #@keys.collect { |k| input[k] }
+    input
+  end
 end
 
 config = YAML::load(File.new("reactor.yaml").read)
 
 data = Hash.new { |h,k| h[k] = [] }
 
-keys = config["key"].split(",")
-action = config["action"]
-
-reactor = Reactor.new
-reactor.keys = keys
-reactor.threshold = action["threshold"]
+reactors = []
+config.each do |c|
+  r = Reactor.new(c)
+  reactors << r
+end
 
 $stdin.each do |line|
   obj = YAML::load(line)
   # Skip unless this object has all keys present
-  next unless keys.collect { |k| obj.has_key?(k) }.reduce { |a,b| a & b }
-
-  # make a new object with only the 'keys' key:value pairs present.
-  shortobj = Hash[*keys.collect { |k| [k, obj[k]] }.flatten ]
-
-  input = ReactorInput.new(shortobj, Time.now)
-  reactor.feed(input)
+  reactors.each { |r| r.feed(obj) }
 end
