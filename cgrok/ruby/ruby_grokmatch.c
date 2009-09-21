c7163b0f2f14b6ae429bd52e83b412f134b01880
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2009-09-21 06:30:24 +0000

    - GrokMatch: Lazily set properties like @start, @end, etc. - Don't generate the @captures anymore, that takes time. - Add GrokMatch#each_capture { |name, value| ... }. This method is much faster   if you only want to iterate over the captures once.
    git-svn-id: http://semicomplete.googlecode.com/svn@2264 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- cgrok/ruby/ruby_grokmatch.c
+++ cgrok/ruby/ruby_grokmatch.c
@@ -4,6 +4,11 @@
 VALUE cGrokMatch;
 extern VALUE cGrok;
 
+static ID id_atend;
+static ID id_atstart;
+static ID id_atcaptures;
+static ID id_atsubject;
+
 static void rGrokMatch_free(void *p);
 
 VALUE rGrokMatch_new(VALUE klass) {
@@ -24,6 +29,20 @@ VALUE rGrokMatch_new_from_grok_match(grok_match_t *gm) {
 }
 
 VALUE rGrokMatch_initialize(VALUE self) {
+  grok_match_t *gm;
+  VALUE captures;
+  
+  Data_Get_Struct(self, grok_match_t, gm);
+
+  /* Set the @captures variable to a hash of array of strings */
+  captures = rb_hash_new();
+  //captures = rb_eval_string("Hash.new { |h,k| h[k] = Array.new }");
+  rb_iv_set(self, "@captures", captures);
+
+  return Qtrue;
+}
+
+VALUE rGrokMatch_each_capture(VALUE self) {
   char *name;
   const char *data;
   int namelen, datalen;
@@ -31,16 +50,56 @@ VALUE rGrokMatch_initialize(VALUE self) {
   grok_match_t *gm;
   VALUE captures;
   ID pred_include;
-  
+
   Data_Get_Struct(self, grok_match_t, gm);
+  captures = rb_iv_get(self, "@captures");
+  pred_include = rb_intern("include?");
+  handle = grok_match_walk_init(gm);
+  while (grok_match_walk_next(gm, handle, &name, &namelen,
+                              &data, &datalen) == 0) {
+    VALUE key, value;
 
-  rb_iv_set(self, "@start", INT2FIX(gm->start));
-  rb_iv_set(self, "@end", INT2FIX(gm->end));
-  rb_iv_set(self, "@subject", rb_str_new2(gm->subject));
+#ifdef _TRIM_KEY_EXCESS_IN_C_
+  /* This section will skip captures of %{FOO} and rename captures of
+   * %{FOO:bar} to just 'bar' */
+    size_t koff = 0;
+    /* there is no 'strcspn' that takes a length, so do it ourselves */
+    while (koff < namelen && name[koff] != ':' && name[koff] != '\0') {
+      koff++;
+    }
 
-  /* Set the @captures variable to a hash of array of strings */
-  captures = rb_hash_new();
-  rb_iv_set(self, "@captures", captures);
+    /* Skip captures that aren't named specially */
+    if (koff == namelen) {
+      //printf("Skipping %.*s\n", namelen, name);
+      continue;
+    }
+
+    koff++;
+
+    key = rb_str_new(name + koff, namelen - koff);
+#endif
+
+    key = rb_str_new(name, namelen);
+    value = rb_str_new(data, datalen);
+
+    // Yield [key, value]
+    rb_yield(rb_ary_new3(2, key, value));
+    //rb_ary_push(ary, value);
+  }
+
+  grok_match_walk_end(gm, handle);
+}
+
+VALUE rGrokMatch_to_hash(VALUE self) {
+  char *name;
+  const char *data;
+  int namelen, datalen;
+  void *handle;
+  grok_match_t *gm;
+  VALUE captures;
+  ID pred_include;
+
+  captures = rb_iv_get(self, "@captures");
 
   pred_include = rb_intern("include?");
   handle = grok_match_walk_init(gm);
@@ -60,6 +119,7 @@ VALUE rGrokMatch_initialize(VALUE self) {
 
     /* Skip captures that aren't named specially */
     if (koff == namelen) {
+      //printf("Skipping %.*s\n", namelen, name);
       continue;
     }
 
@@ -74,7 +134,8 @@ VALUE rGrokMatch_initialize(VALUE self) {
       rb_hash_aset(captures, key, rb_ary_new());
     }
 
-    ary = rb_hash_aref(captures, key);
+    //ary = rb_hash_aref(captures, key);
+    ary = rb_hash_lookup(captures, key);
     rb_ary_push(ary, value);
   }
 
@@ -88,15 +149,26 @@ VALUE rGrokMatch_captures(VALUE self) {
 }
 
 VALUE rGrokMatch_start(VALUE self) {
-  return rb_iv_get(self, "@start");
+  grok_match_t *gm;
+  Data_Get_Struct(self, grok_match_t, gm);
+  return INT2FIX(gm->start);
 }
 
 VALUE rGrokMatch_end(VALUE self) {
-  return rb_iv_get(self, "@end");
+  grok_match_t *gm;
+  Data_Get_Struct(self, grok_match_t, gm);
+  VALUE ret = Qnil;
+  ret = rb_iv_get(self, "@end");
+  if (ret == Qnil) {
+    ret = rb_iv_set(self, "@end", INT2FIX(gm->end));
+  }
+  return INT2FIX(gm->end);
 }
 
 VALUE rGrokMatch_subject(VALUE self) {
-  return rb_iv_get(self, "@subject");
+  grok_match_t *gm;
+  Data_Get_Struct(self, grok_match_t, gm);
+  return rb_str_new2(gm->subject);
 }
 
 void rGrokMatch_free(void *p) {
@@ -111,4 +183,8 @@ void Init_GrokMatch() {
   rb_define_method(cGrokMatch, "start", rGrokMatch_start, 0);
   rb_define_method(cGrokMatch, "end", rGrokMatch_end, 0);
   rb_define_method(cGrokMatch, "subject", rGrokMatch_subject, 0);
+  rb_define_method(cGrokMatch, "each_capture", rGrokMatch_each_capture, 0);
+  id_atend = rb_intern("@end");
+  id_atstart = rb_intern("@start");
+  id_atcaptures = rb_intern("@captures");
 }
