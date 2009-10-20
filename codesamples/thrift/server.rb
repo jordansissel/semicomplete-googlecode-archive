0fafc7b0d13d0ac87eeedba7856c32d8225461f8
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2009-10-20 07:58:14 +0000

    - try threadpoolserver
    git-svn-id: http://semicomplete.googlecode.com/svn@2326 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- codesamples/thrift/server.rb
+++ codesamples/thrift/server.rb
@@ -6,7 +6,8 @@ require 'user_storage'
 
 class UserStorageHandler
   def store(user)
-    puts user
+    puts Thread.current
+    puts user.inspect
   end
 end
 
@@ -14,7 +15,8 @@ handler = UserStorageHandler.new()
 processor = UserStorage::Processor.new(handler)
 transport = Thrift::ServerSocket.new(9090)
 transportFactory = Thrift::BufferedTransportFactory.new()
-server = Thrift::SimpleServer.new(processor, transport, transportFactory)
+#server = Thrift::SimpleServer.new(processor, transport, transportFactory)
+server = Thrift::ThreadPoolServer.new(processor, transport, transportFactory)
 puts "Starting the server..."
 server.serve()
 puts "Done"
