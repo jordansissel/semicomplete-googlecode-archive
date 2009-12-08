63a314b7afdfec6d00e807d1fc28b3239ae96a18
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2009-12-08 09:37:35 +0000

    - make the alarm more resilient
    git-svn-id: http://semicomplete.googlecode.com/svn@2480 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- sysadvent/2009/day08/alarm.rb
+++ sysadvent/2009/day08/alarm.rb
@@ -9,13 +9,23 @@ end
 timeout = ARGV[0].to_f
 cmd = ARGV[1..-1]
 
+pid = nil
 begin
   Timeout.timeout(timeout) do
-    system(*cmd)
+    pid = fork do
+      exec(*cmd);
+      exit 1 # in case exec() fails
+    end
+    Process.waitpid(pid)
+    pid = nil
     exit $?.exitstatus
   end
 rescue Timeout::Error
   STDERR.puts "Execution expired (timeout == #{timeout})"
+  # kill the process if it's still alive.
+  if pid
+    Process.kill('TERM', pid)
+  end
   exit 254
 end
 
