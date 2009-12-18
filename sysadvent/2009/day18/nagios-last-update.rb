1b5f985e450ace8c260d766d1a53de9858e9a540
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2009-12-18 09:49:11 +0000

    - don't use ruby test/unit anymore - Catch case when time is "N/A" (nagios has just started)
    git-svn-id: http://semicomplete.googlecode.com/svn@2527 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- sysadvent/2009/day18/nagios-last-update.rb
+++ sysadvent/2009/day18/nagios-last-update.rb
@@ -11,35 +11,49 @@
 require 'rubygems'
 require 'mechanize'
 require 'date'
-require 'test/unit'
 
-class NagiosHealthTest < Test::Unit::TestCase
-  def test_nagios_last_command_check_age
-    nagioshost = ARGV[0]
-    maxage = ARGV[1].to_f
+def warn(message)
+  puts message
+  exit 1
+end
 
-    mech = WWW::Mechanize.new
-    page = mech.get("http://#{nagioshost}/nagios/cgi-bin/extinfo.cgi?type=0")
+def critical(message)
+  puts message
+  exit 2
+end
 
-    # find the row like this:
-    # <TR><TD CLASS='dataVar'>Last External Command Check:</TD>
-    #     <TD CLASS='dataVal'>12-18-2009 03:47:22</TD></TR>
-    # And get that 2nd column.
-    rowtext = "Last External Command Check:"
-    xpath = "//tr[contains(td/text(), '#{rowtext}')]/td[@class='dataVal']/text()"
-    last_update_str = page.search(xpath).to_s
+nagioshost = ARGV[0]
+maxage = ARGV[1].to_f
 
-    now = Time.now
-    last_update_time = Time.parse(DateTime.strptime(last_update_str,
-                                                    "%m-%d-%Y %H:%M:%S").to_s)
+mech = WWW::Mechanize.new
+page = mech.get("http://#{nagioshost}/nagios/cgi-bin/extinfo.cgi?type=0")
 
-    # Since the time reported by nagios doesn't include a zone, and parsing it
-    # defaults to GMT, add local timezone offset.
-    last_update_time -= now.gmt_offset
+# find the row like this:
+# <TR><TD CLASS='dataVar'>Last External Command Check:</TD>
+#     <TD CLASS='dataVal'>12-18-2009 03:47:22</TD></TR>
+# And get that 2nd column.
+rowtext = "Last External Command Check:"
+xpath = "//tr[contains(td/text(), '#{rowtext}')]/td[@class='dataVal']/text()"
+last_update_str = page.search(xpath).to_s
+case last_update_str
+when ""
+  critical "extinfo.cgi returned bad data. Nagios is probably down?"
+when "N/A"
+  warn "last external command time is 'N/A'. Nagios may have just restarted."
+end
 
-    # Require last check run to be less than 15 minutes ago.
-    age = now - last_update_time
-    assert(age < maxage, 
-           "Time of last Nagios check is older than #{maxage} seconds.")
-  end # def test_nagios_last_command_check_age
-end # class NagiosHealthTest
+now = Time.now
+last_update_time = Time.parse(DateTime.strptime(last_update_str,
+                                                "%m-%d-%Y %H:%M:%S").to_s)
+
+# Since the time reported by nagios doesn't include a zone, and parsing it
+# defaults to GMT, add local timezone offset.
+last_update_time -= now.gmt_offset
+
+# Require last check run to be less than 15 minutes ago.
+age = now - last_update_time
+if age > maxage
+  critical "Time of last Nagios check is older than #{maxage} seconds."
+end
+
+puts "OK. Nagios last-update #{age} seconds ago."
