b7db0e73b401c4f30698f5e32ac6dd2b4c0e2f35
jordansissel <jordansissel@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2009-12-28 02:15:37 +0000

    git-svn-id: http://semicomplete.googlecode.com/svn@2545 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- tools/run-xterm.sh
+++ tools/run-xterm.sh
@@ -16,6 +16,9 @@ export STY=
 # Pick our font.
 FONT="*suxus*"
 
+# Generate two random colors. One dark for background, one a lighter version of
+# the dark color. Eval the output and we'll have $BG and $FG as RGB hex values
+# for colors to use.
 eval $(perl -le 'sub c { int(rand() * $ENV{RANGE} + $ENV{OFFSET}) }; sub p { printf("%s=#%02x%02x%02x\n", @_) }; @f = (c, c, c); @b = map($_ - $ENV{OFFSET}, @f);  p("FG",@f); p("BG", @b);')
 
 exec xterm -fg white -ms "$FG" -bg "$BG" -fn "$FONT" -cr green -e "$@"
