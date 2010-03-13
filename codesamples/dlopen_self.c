ef90866c8d56c88cca8274e465660f2ccf36c9d2
jls@semicomplete.com <jls@semicomplete.com@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2010-03-13 09:19:00 +0000

    - Add more random stuff
    git-svn-id: http://semicomplete.googlecode.com/svn@2733 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- codesamples/dlopen_self.c
+++ codesamples/dlopen_self.c
@@ -11,12 +11,13 @@ int main() {
   void *handle = NULL;
   void (*myfunc)(void) = NULL;
 
-  handle = dlopen(NULL, RTLD_LAZY);
-  myfunc = dlsym(handle, "foo");
-  if (myfunc == NULL) {
-    printf("e: %s\n", dlerror());
-  } else {
-    myfunc();
-  }
+  //handle = dlopen(NULL, RTLD_LAZY);
+  handle = dlopen("/lib64/libc.so.6", RTLD_LAZY);
+  //myfunc = dlsym(handle, "foo");
+  //if (myfunc == NULL) {
+    //printf("e: %s\n", dlerror());
+  //} else {
+    //myfunc();
+  //}
   return 0;
 }
