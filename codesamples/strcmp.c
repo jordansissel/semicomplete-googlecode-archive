5917ede35745603ac59fa4ef37a91377bf39ef99
jls@semicomplete.com <jls@semicomplete.com@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2010-06-13 00:23:44 +0000

    git-svn-id: http://semicomplete.googlecode.com/svn@2938 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- codesamples/strcmp.c
+++ codesamples/strcmp.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 
 int main() {
-  printf("%d\n", strcmp("testt", "test"));
+
+  printf("%d\n", strcmp("--foo", "--"));
+  printf("%d\n", strcmp("--", "--foo"));
+  printf("%d\n", strcmp("--", "--"));
   return 0;
 }
