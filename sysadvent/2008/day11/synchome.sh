7f5238a5cbb58236fff6e7e1c587ce866be3baaf
jls@semicomplete.com <jls@semicomplete.com@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2010-08-25 01:32:32 +0000

    - refactor rsync path to a variable ($source)
    git-svn-id: http://semicomplete.googlecode.com/svn@3005 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- sysadvent/2008/day11/synchome.sh
+++ sysadvent/2008/day11/synchome.sh
@@ -10,10 +10,13 @@
 # 'sed -r' is GNU specific, if you're on FreeBSD (or familiy) you'll want
 # to use 'sed -E'
 
-prog="$(basename $0)"
+# set this to the path to your rsync server and path where you keep
+# your homedirs
+source="someserver::homedirs"
 
+prog="$(basename $0)"
 # Make sure someserver::homedirs rsync module exists
-rsync -q someserver::homedirs
+rsync -q $source
 if [ $? -ne 0 ] ; then
   echo "$prog: rsync fatal error" >&2
   exit 1
@@ -22,9 +25,8 @@ fi
 # For all directories in someserver::homedir that are not dot-directories,
 # assume they are a user and try to sync files to their homedir.
 
-rsync someserver::homedirs | awk '$NF !~ /^\./ { print $NF }' \
+rsync $source | awk '/^d/ && $NF !~ /^\./ { print $NF }' \
 | while read user; do
-
   # If the user doesn't exist on this system, skip it.
   getent passwd $user > /dev/null
   if [ $? -ne 0 ] ; then
@@ -40,6 +42,6 @@ rsync someserver::homedirs | awk '$NF !~ /^\./ { print $NF }' \
   fi
   
   # run rsync as $user to prevent accidents
-  su -c "rsync -vr --exclude=.svn someserver::homedirs/$user/ /home/$user/" $user
+  su -c "rsync -vr --exclude=.svn $source/$user/ /home/$user/" $user
 done
 
