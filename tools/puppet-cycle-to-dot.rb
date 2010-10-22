9dc7169e29f7f2d20bed3cfd0ba3ab34e1b8fadc
jls@semicomplete.com <jls@semicomplete.com@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2010-10-22 00:17:54 +0000

    - Add tool for turning puppet dependency cycle error output into a viewable graph
    git-svn-id: http://semicomplete.googlecode.com/svn@3076 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ tools/puppet-cycle-to-dot.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+#
+# Reads from stdin.
+# The input is expected to match puppet's error output when there is a
+# dependency cycle found.
+# 
+# The output looks like this:
+#
+# """
+# err: Could not apply complete catalog: Found dependency cycles in the
+# following relationships: < list of relationships >; try using the '--graph'
+# option ...
+# """
+#
+# The --graph option generates a graph with all relationships and is usually
+# quite hard to read.
+# This script will take the error output which only includes the cycles and 
+# give you a .dot output that is reasonable.
+#
+# Take the output and use graphviz (for example) and convert to pdf for view:
+#   Like 'circo' circular graph display:
+#   % circo -Tpdf yourdotfile > cycles.pdf
+
+require "rubygems"
+require "erb"
+require "ap"
+
+data = $stdin.read
+data.gsub!(/^err:.*: /, "")
+data.gsub!(/; try using the '--graph'.*/, "")
+
+edges = data.split(/, */).collect { |e| e.chomp }
+verteces = edges.collect { |edge| edge.split(" => ") }.flatten.collect { |v| v.chomp }
+
+template = ERB.new <<-ERB
+digraph Dependencies {
+  label = "Dependencies"
+  <% verteces.each do |vertex| %>
+  "<%= vertex %>" [
+    fontsize = 8,
+    label = "<%= vertex %>"
+  ]
+  <% end %>
+
+  <% edges.each do |edge| %>
+  <%   left, right = edge.split(" => ") %>
+  "<%= left %>" -> "<%= right %>" [
+    fontsize = 8
+  ]
+  <% end %>
+}
+ERB
+
+puts template.result(binding)
